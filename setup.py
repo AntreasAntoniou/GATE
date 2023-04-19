353603b9f495e8832b93a561262161c7e904d7b7
Antreas Antoniou <iam@antreas.io>
2023-04-19 01:26:22 +0100

    Update

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name="tali_wit",
+    name="gate",
     version="2.0.0",
     description="A minimal, stateless, machine learning research template for PyTorch",
     author="Antreas Antoniou",
     author_email="iam@antreas.io",
-    packages=["tali_wit"],
+    packages=["gate"],
 )
