799cd3816b47f901e340eaaa64736de505bb97c0
Antreas Antoniou <iam@antreas.io>
2023-04-27 02:55:00 +0100

    Update

---
+++ tests/data/image/segmentation/test_ade20k_segmentation.py
@@ -0,0 +1,27 @@
+# test_food101_dataset.py
+import pytest
+from gate.data.image.segmentation.ade20k import build_ade20k_dataset
+import os
+
+
+def test_build_ade20k_dataset():
+    # Test if the function returns the correct dataset split
+
+    train_set = build_ade20k_dataset(
+        "train", data_dir=os.environ.get("TEST_DIR")
+    )
+    assert train_set is not None, "Train set should not be None"
+
+    val_set = build_ade20k_dataset("val", data_dir=os.environ.get("TEST_DIR"))
+    assert val_set is not None, "Validation set should not be None"
+
+    test_set = build_ade20k_dataset(
+        "test", data_dir=os.environ.get("TEST_DIR")
+    )
+    assert test_set is not None, "Test set should not be None"
+
+    # Test if the function raises an error when an invalid set_name is given
+    with pytest.raises(KeyError):
+        build_ade20k_dataset(
+            "invalid_set_name", data_dir=os.environ.get("TEST_DIR")
+        )
