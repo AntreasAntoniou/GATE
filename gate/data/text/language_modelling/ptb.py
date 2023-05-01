6da063e61434fadc43fab210414bdd4de4e91f8e
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2023-05-01 23:58:06 +0100

    add ptb dataset with tests

---
+++ gate/data/text/language_modelling/ptb.py
@@ -0,0 +1,45 @@
+# ptb.py
+from typing import Optional
+
+import numpy as np
+from datasets import load_dataset
+
+
+def build_ptb_dataset(
+    set_name: str, data_dir: Optional[str] = None
+) -> dict:
+    """
+    Build a PTB dataset using the Hugging Face datasets library.
+    https://huggingface.co/datasets/ptb_text_only
+
+    Args:
+        data_dir: The directory where the dataset cache is stored.
+        set_name: The name of the dataset split to return
+        ("train", "val", or "test").
+
+    Returns:
+        A dictionary containing the dataset split.
+    """
+    rng = np.random.RandomState(42)
+
+    train_data = load_dataset(
+        path="ptb_text_only",
+        split="train",
+        cache_dir=data_dir,
+    )
+
+    val_data = load_dataset(
+        path="ptb_text_only",
+        split="validation",
+        cache_dir=data_dir,
+    )
+
+    test_data = load_dataset(
+        path="ptb_text_only",
+        split="test",
+        cache_dir=data_dir,
+    )
+
+    dataset_dict = {"train": train_data, "val": val_data, "test": test_data}
+
+    return dataset_dict[set_name]
