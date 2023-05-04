fded9463fa87a7420690b26ccd129cb311c3f4c7
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2023-05-04 18:40:36 +0100

    add trivia qa

--- gate/data/text/__init__.py
+++ gate/data/text/__init__.py
@@ -2,4 +2,5 @@ from .language_modelling import *
 from .classification import *
 from .reading_comprehension import *
 from .fact_checking import *
+from .question_answering import *
 from .common_sense import *
