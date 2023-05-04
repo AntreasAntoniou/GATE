9145df3174cb117f8355be6f84cec905e00564aa
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2023-05-04 15:51:57 +0100

    add fact checking dataset multi_fc

--- gate/data/text/__init__.py
+++ gate/data/text/__init__.py
@@ -1,3 +1,4 @@
 from .language_modelling import *
 from .classification import *
 from .reading_comprehension import *
+from .fact_checking import *
---
+++ tests/data/text/fact_checking/test_multi_fc_fact_checking.py
@@ -0,0 +1,29 @@
+# test_multi_fc_reding_comprehension.py
+import os
+
+import pytest
+
+from gate.data.text.fact_checking.multi_fc import build_multi_fc_dataset
+
+
+def test_build_multi_fc_dataset():
+    # Test if the function returns the correct dataset split
+
+    train_set = build_multi_fc_dataset("train", data_dir=os.environ.get("TEST_DIR"))
+    assert train_set is not None, "Train set should not be None"
+
+    val_set = build_multi_fc_dataset("val", data_dir=os.environ.get("TEST_DIR"))
+    assert val_set is not None, "Validation set should not be None"
+
+    test_set = build_multi_fc_dataset("test", data_dir=os.environ.get("TEST_DIR"))
+    assert test_set is not None, "Test set should not be None"
+
+    # Test if the function raises an error when an invalid set_name is given
+    with pytest.raises(KeyError):
+        build_multi_fc_dataset(
+            "invalid_set_name", data_dir=os.environ.get("TEST_DIR")
+        )
+
+
+if __name__ == "__main__":
+    test_build_multi_fc_dataset()
