ec3ddd2d16c086d24fab9fdd008d3950b00064b5
AntreasAntoniou <iam@antreas.io>
2023-05-08 19:12:08 +0000

    feat: Add functionality to upload images to Imgur
    This commit adds the ability to upload an image to Imgur using the
    ImgurUploader library, and applies the custom pad_image transform to the
    image before uploading.

--- playground.py
+++ playground.py
@@ -1,3 +1,5 @@
+from imguruploader import ImgurUploader
+import os
 import torchvision.transforms as transforms
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -11,19 +13,41 @@ def pad_image(image, target_size=224):
     return transforms.functional.pad(image, padding, fill=0)
 
 
-# Load your 32x32 image as a PIL image
-image_path = "path/to/your/image.jpg"
-image = Image.open(image_path)
+def create_sample_image(size=(32, 32)):
+    image = Image.new("RGB", size, color=(255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
+    for i in range(0, size[0], 8):
+        for j in range(0, size[1], 8):
+            draw.rectangle(
+                [i, j, i + 7, j + 7], fill=colors[((i // 8) + (j // 8)) % 3]
+            )
+
+    return image
+
+
+def upload_to_imgur(image, client_id):
+    uploader = ImgurUploader(client_id)
+    image_path = "temp_image.png"
+    image.save(image_path)
+    response = uploader.upload_image(image_path)
+    os.remove(image_path)
+    return response["link"]
+
+
+client_id = "4fa27e2baf27c1e"  # Replace with your actual Imgur Client ID
+
+# Create a 32x32 sample image
+image = create_sample_image()
+
 
 # Apply the custom pad_image transform
 padded_image = pad_image(image, target_size=224)
 
-# Visualize the original and padded images
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
-ax1.imshow(image)
-ax1.set_title("Original Image")
-ax1.axis("off")
-ax2.imshow(padded_image)
-ax2.set_title("Padded Image")
-ax2.axis("off")
-plt.show()
+# Upload the original and padded images to Imgur
+image_url = upload_to_imgur(image, client_id)
+padded_image_url = upload_to_imgur(padded_image, client_id)
+
+print("Original image URL:", image_url)
+print("Padded image URL:", padded_image_url)
