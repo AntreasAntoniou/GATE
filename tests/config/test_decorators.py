6d88931cfaf4743784f6b2f1f41aaab0c2a27864
AntreasAntoniou <iam@antreas.io>
2023-05-08 01:50:32 +0000

    updates

--- tests/config/test_decorators.py
+++ tests/config/test_decorators.py
@@ -14,7 +14,7 @@ def test_configurable_and_register_configurables():
     config_store = register_configurables("gate", config_store)
 
     # Retrieve the configuration from the config store
-    config = config_store.load("test_group")
+    config = config_store.test_group
 
     # Check if the configuration is loaded correctly
     assert config["test_function"]["a"] == 1
