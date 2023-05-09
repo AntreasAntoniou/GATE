21ba9652c514d34af87daf7a6c62f21b3ad795c1
AntreasAntoniou <iam@antreas.io>
2023-05-09 03:33:43 +0000

    updates

--- gate/models/task_adapters/classification.py
+++ gate/models/task_adapters/classification.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, Optional
 
 import torch.nn as nn
+import torch
 
 
 class BackboneWithLinear(nn.Module):
@@ -9,7 +10,28 @@ class BackboneWithLinear(nn.Module):
         self.model = model
         self.linear = nn.Linear(num_clip_features, num_classes)
 
-    def forward(self, input_dict: Dict):
-        x = self.model(**input_dict)
+    def forward(
+        self,
+        input_dict: Optional[Dict] = None,
+        image: Optional[torch.Tensor] = None,
+        text: Optional[torch.Tensor] = None,
+        audio: Optional[torch.Tensor] = None,
+        video: Optional[torch.Tensor] = None,
+    ) -> Dict[str, torch.Tensor]:
+        if input_dict is not None:
+            x = self.model(**input_dict)
+
+        if image is not None:
+            x = self.model(image=image)
+
+        if text is not None:
+            x = self.model(text=text)
+
+        if audio is not None:
+            x = self.model(audio=audio)
+
+        if video is not None:
+            x = self.model(video=video)
+
         x = self.linear(x)
         return x
