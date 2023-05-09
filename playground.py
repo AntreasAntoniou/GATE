f1a3c9d77716461b3065b8a91aa745013138c4f1
AntreasAntoniou <iam@antreas.io>
2023-05-09 20:28:12 +0000

    updates

--- playground.py
+++ playground.py
@@ -1,32 +1,13 @@
-from urllib.request import urlopen
-from PIL import Image
-import timm
-
-img = Image.open(
-    urlopen(
-        "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png"
-    )
+import os
+from gate.data.image.visual_question_answering.ok_vqa import (
+    build_ok_vqa_dataset,
 )
 
-model = timm.create_model(
-    "resnet50.a1_in1k",
-    pretrained=True,
-    num_classes=0,  # remove classifier nn.Linear
+
+train_set = build_ok_vqa_dataset(
+    "train", data_dir=os.environ.get("PYTEST_DIR")
 )
-model = model.eval()
 
-# get model specific transforms (normalization, resize)
-data_config = timm.data.resolve_model_data_config(model)
-transforms = timm.data.create_transform(**data_config, is_training=False)
-
-output = model(
-    transforms(img).unsqueeze(0)
-)  # output is (batch_size, num_features) shaped tensor
-
-# or equivalently (without needing to set num_classes=0)
-
-output = model.forward_features(transforms(img).unsqueeze(0))
-# output is unpooled, a (1, 2048, 7, 7) shaped tensor
-
-output = model.forward_head(output, pre_logits=True)
-# output is a (1, num_features) shaped tensor
+for item in train_set:
+    print(item)
+    break
