129d81a9f37f10ebfad7a726828e29504b4792ca
AntreasAntoniou <iam@antreas.io>
2023-05-09 21:36:36 +0000

    updates

--- playground.py
+++ playground.py
@@ -16,14 +16,35 @@ vqa_train_vqa = build_vqa_v2_dataset(
     "train", data_dir=os.environ.get("PYTEST_DIR")
 )
 
-for item in ok_train_set:
-    print(item)
-    break
+# for item in ok_train_set:
+#     print(item)
+#     break
 
-print(
+# print(
     "-------------------------------------------------------------------------------------------"
 )
 
-for item in vqa_train_vqa:
+for idx, item in enumerate(vqa_train_vqa):
     print(item)
-    break
+    if idx == 5:
+        break
+    
+
+
+def select_okvqa_items(data_dict):
+    """
+    Selects the necessary keys from an OK-VQA dataset entry.
+
+    :param data_dict: A dictionary containing an entry from the OK-VQA dataset.
+    :type data_dict: dict
+
+    :return: A dictionary containing only the selected keys and their corresponding values.
+    :rtype: dict
+    """
+    return {
+        "image_id": data_dict["id_image"],
+        "image": data_dict["image"],
+        "question_id": data_dict["id_question"],
+        "question": data_dict["question"],
+        "answers": data_dict["answers"],
+    }
