4c0b7a2f5b7650ce8d8feabc7466982c79b4c1c0
AntreasAntoniou <iam@antreas.io>
2023-05-09 20:33:19 +0000

    updates

--- playground.py
+++ playground.py
@@ -3,12 +3,27 @@ from rich import print
 from gate.data.image.visual_question_answering.ok_vqa import (
     build_ok_vqa_dataset,
 )
+from gate.data.image.visual_question_answering.vqa_v2 import (
+    build_vqa_v2_dataset,
+)
 
 
-train_set = build_ok_vqa_dataset(
+ok_train_set = build_ok_vqa_dataset(
     "train", data_dir=os.environ.get("PYTEST_DIR")
 )
 
-for item in train_set:
+vqa_train_vqa = build_vqa_v2_dataset(
+    "train", data_dir=os.environ.get("PYTEST_DIR")
+)
+
+for item in ok_train_set:
+    print(item)
+    break
+
+print(
+    "-------------------------------------------------------------------------------------------"
+)
+
+for item in vqa_train_vqa:
     print(item)
     break
