89ca47652f78476f18d3a5fb947d0c994153782a
AntreasAntoniou <iam@antreas.io>
2023-05-09 19:41:40 +0000

    updates

--- playground.py
+++ playground.py
@@ -1,5 +1,32 @@
-from rich import print
-from gate.trainers.classification import ClassificationTrainer
+from urllib.request import urlopen
+from PIL import Image
+import timm
 
+img = Image.open(
+    urlopen(
+        "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png"
+    )
+)
 
-print(ClassificationTrainer.__dict__)
+model = timm.create_model(
+    "resnet50.a1_in1k",
+    pretrained=True,
+    num_classes=0,  # remove classifier nn.Linear
+)
+model = model.eval()
+
+# get model specific transforms (normalization, resize)
+data_config = timm.data.resolve_model_data_config(model)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+output = model(
+    transforms(img).unsqueeze(0)
+)  # output is (batch_size, num_features) shaped tensor
+
+# or equivalently (without needing to set num_classes=0)
+
+output = model.forward_features(transforms(img).unsqueeze(0))
+# output is unpooled, a (1, 2048, 7, 7) shaped tensor
+
+output = model.forward_head(output, pre_logits=True)
+# output is a (1, num_features) shaped tensor
