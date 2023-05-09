f6a4228d5ab5bb85a53e1d635d424ce26704161d
AntreasAntoniou <iam@antreas.io>
2023-05-09 02:25:53 +0000

    feat: add support for TALINet backbones
    This commit adds support for TALINet backbones for image, text, audio, and video modalities in classification tasks. It also includes helper methods for creating a model with a linear layer and for extracting transformation functions from the backbone model.

--- gate/models/classification/talinet.py
+++ gate/models/classification/talinet.py
@@ -1,108 +1,99 @@
-from gate.models import ModelAndTransform
 from dataclasses import dataclass
-from typing import Any, Dict, Tuple, Union
-
-import torch
-import torch.nn as nn
+from typing import Any, Dict, Union
 
 from gate.boilerplate.decorators import configurable
 from gate.config.variables import HYDRATED_NUM_CLASSES
 from gate.models import ModelAndTransform
-from gate.models.backbones.clip import CLIPAdapter
+from gate.models.backbones.talinet import TALINet
 from gate.models.core import (
     GATEModel,
     SourceModalityConfig,
     TargetModalityConfig,
 )
-from gate.models.task_adapters.classification import (
-    BackboneWithLinear,
-)
+from gate.models.task_adapters.classification import BackboneWithLinear
+
+
+SUPPORTED_MODALITIES = ["image", "text", "audio", "video"]
+
+
+def create_model_with_linear(backbone_model, num_features, num_classes):
+    """
+    Helper function to create a model with linear layer.
+    """
+    return BackboneWithLinear(backbone_model, num_features, num_classes)
 
 
 def build_model(
-    model_name: str = "openai/clip-vit-base-patch16",
+    clip_model_name: str = "openai/clip-vit-base-patch16",
+    whisper_model_name: str = "openai/whisper-small",
     modality: str = "image",
     pretrained: bool = True,
     num_classes: int = 100,
 ) -> ModelAndTransform:
-    """
-    🏗️ Build the model using the Hugging Face transformers library.
+    backbone_model = TALINet(
+        clip_model_name=clip_model_name,
+        whisper_model_name=whisper_model_name,
+        pretrained=pretrained,
+    )
 
-    :param model_name: The name of the model to load.
-    :param pretrained: Whether to use a pretrained model.
-    :param num_classes: The number of classes for the linear layer.
-    :return: A ModelAndTransform instance containing the model and transform function.
-    """
-    backbone_model = CLIPAdapter(model_name=model_name, pretrained=pretrained)
-    if modality == "image":
-        model = BackboneWithLinear(
-            backbone_model, backbone_model.clip.vision_embed_dim, num_classes
-        )
-    elif modality == "text":
-        model = BackboneWithLinear(
-            backbone_model, backbone_model.clip.text_embed_dim, num_classes
-        )
-    else:
+    # Check if modality is supported, if not raise a ValueError.
+    if modality not in SUPPORTED_MODALITIES:
         raise ValueError(f"Modality {modality} not supported for CLIP.")
+
+    num_features_attr = f"{modality}_num_features"
+    num_features = getattr(backbone_model, num_features_attr, None)
+    model = create_model_with_linear(backbone_model, num_features, num_classes)
 
     if not pretrained:
         model.init_weights()
 
-    transform = lambda image: backbone_model.preprocessor(
-        images=image, return_tensors="pt"
-    )
+    transform_dict = backbone_model.get_transforms()
 
     def transform_wrapper(inputs: Union[Dict, Any]):
-        output_dict = {}
-
-        if "image" in inputs:
-            output_dict["image"] = transform(inputs["image"])["pixel_values"][
-                0
-            ]
-        if "text" in inputs:
-            output_dict["text"] = transform(inputs["text"])["input_ids"][0]
-
-        if "labels" in inputs:
-            output_dict["labels"] = inputs["labels"]
-
-        return output_dict
+        return {
+            key: transform_dict[key](value)
+            if key in SUPPORTED_MODALITIES
+            else inputs["labels"]
+            for key, value in inputs.items()
+        }
 
     return ModelAndTransform(model=model, transform=transform_wrapper)
 
 
 @configurable(
     group="model",
-    name="clip-classification",
+    name="tali-classification",
     defaults=dict(num_classes=HYDRATED_NUM_CLASSES),
 )
-def build_gate_clip_model(
-    model_name: str = "openai/clip-vit-base-patch16",
+def build_gate_tali_model(
+    clip_model_name: str = "openai/clip-vit-base-patch16",
+    whisper_model_name: str = "openai/whisper-small",
     modality: str = "image",
     pretrained: bool = True,
     num_classes: int = 100,
 ):
     model_and_transform = build_model(
-        model_name=model_name,
+        clip_model_name=clip_model_name,
+        whisper_model_name=whisper_model_name,
         pretrained=pretrained,
         num_classes=num_classes,
         modality=modality,
     )
-    if modality == "image":
-        model_modality_config_image_classification = TargetModalityConfig(
-            image=[SourceModalityConfig(image=True)]
-        )
-    elif modality == "text":
-        model_modality_config_image_classification = TargetModalityConfig(
-            text=[SourceModalityConfig(text=True)]
-        )
+
+    # Create Modality Config based on input modality
+    model_modality_config = TargetModalityConfig(
+        **{modality: [SourceModalityConfig(**{modality: True})]}
+    )
 
     model_key_remapper_dict_config = {
         "image": "image",
         "text": "image",
+        "audio": "audio",
+        "video": "video",
     }
 
     gate_model = GATEModel(
-        config=model_modality_config_image_classification,
+        config=model_modality_config,
         model=model_and_transform.model,
         key_remapper_dict=model_key_remapper_dict_config,
     )
