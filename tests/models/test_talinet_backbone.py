ae5679fc4c8ddacd5b0bfcbd86772b2eebb3e936
AntreasAntoniou <iam@antreas.io>
2023-05-09 03:50:16 +0000

    updates

--- tests/models/test_talinet_backbone.py
+++ tests/models/test_talinet_backbone.py
@@ -11,7 +11,7 @@ def test_talinet_forward():
 
     # Generate some fake input data
     image = torch.randn(1, 3, 224, 224)
-    text = torch.randint(0, 100, (1, 512))
+    text = torch.randint(0, 100, (1, 77))
     audio = torch.randn(1, 44100)  # adjust dimensions as needed
     video = torch.randn(1, 3, 224, 224)  # adjust dimensions as needed
 
