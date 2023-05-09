e6372a08243645d181dda99008455ea6c603f94b
AntreasAntoniou <iam@antreas.io>
2023-05-09 03:55:37 +0000

    updates

--- tests/models/test_talinet_backbone.py
+++ tests/models/test_talinet_backbone.py
@@ -8,15 +8,22 @@ from gate.models.backbones.talinet import (
 def test_talinet_forward():
     # Instantiate a TALINet object
     model = TALINet()
-
+    transforms = model.get_transforms()
     # Generate some fake input data
     image = torch.randn(1, 3, 224, 224)
-    text = torch.randint(0, 100, (1, 77))
-    audio = torch.randn(1, 16000)  # adjust dimensions as needed
+    text = torch.randint(0, 100, ["Hello my dude let's do a test"])
+    audio = torch.randn(1, 44000)  # adjust dimensions as needed
     video = torch.randn(1, 3, 224, 224)  # adjust dimensions as needed
 
+    input_dict = {
+        "image": transforms["image"](image),
+        "text": transforms["text"](text),
+        "audio": transforms["audio"](audio),
+        "video": transforms["video"](video),
+    }
+
     # Call the forward method
-    output = model.forward(image=image, text=text, audio=audio, video=video)
+    output = model.forward(**input_dict)
 
     # Check that the output is a dictionary
     assert isinstance(output, dict)
