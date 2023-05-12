a19e063eb5fd98b4bb0130e4424304047d9764db
AntreasAntoniou <iam@antreas.io>
2023-05-12 23:20:47 +0000

    updates

--- gate/trainers/visual_question_answering.py
+++ gate/trainers/visual_question_answering.py
@@ -43,8 +43,8 @@ class VQATrainer(Trainer):
 
         # Generate answers and get the ground truth
         predicted_answers = model.model.generate_text(**batch)
-        ground_truth_answers = batch[
-            "answers"
+        ground_truth_answers = batch["text"][
+            "answer_original"
         ]  # Assuming this is where the true answers are
 
         # Prepare data for VQA evaluation
