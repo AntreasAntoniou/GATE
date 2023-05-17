d883deff7d32a77f403740601fb65034a980d7af
Antreas Antoniou <iam@antreas.io>
2023-05-17 02:54:24 +0000

    updates

--- gate/data/medical/classification/ham10k.py
+++ gate/data/medical/classification/ham10k.py
@@ -13,44 +13,13 @@ import numpy as np
 from gate.boilerplate.decorators import configurable
 from gate.boilerplate.utils import count_files_recursive, get_logger
 from gate.config.variables import DATASET_DIR
+from gate.data import download_kaggle_dataset
 from gate.data.core import GATEDataset
 from gate.data.tasks.classification import ClassificationTask
 
 logger = get_logger(name=__name__, set_rich=True)
 
 FILE_COUNT_AFTER_DOWNLOAD_AND_EXTRACT = 70236
-
-
-def download_kaggle_dataset(
-    dataset_name: str,
-    dataset_path: str,
-    target_dir_path: pathlib.Path,
-    file_count_after_download_and_extract: Optional[int],
-):
-    # Initialize the Kaggle API client
-    dataset_download_path = pathlib.Path(target_dir_path) / dataset_name
-    if (
-        pathlib.Path(dataset_download_path).exists()
-        and count_files_recursive(dataset_download_path)
-        == file_count_after_download_and_extract
-    ):
-        logger.info(f"Dataset directory {target_dir_path} already exists.")
-        return {"dataset_download_path": dataset_download_path}
-
-    api = KaggleApi()
-    # Ensure the directory for the dataset exists
-
-    api.authenticate()
-
-    # Download the dataset
-    api.dataset_download_files(
-        dataset_path,
-        path=dataset_download_path,
-        unzip=True,
-        quiet=False,
-        force=True,
-    )
-    return {"dataset_download_path": dataset_download_path}
 
 
 class HAM10KClassification(Dataset):
