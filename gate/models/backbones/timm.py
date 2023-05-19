282b8a41dc535878a98a51dfff2ea2c93bf73a89
Antreas Antoniou <iam@antreas.io>
2023-05-19 18:17:59 +0000

    update

--- gate/models/backbones/timm.py
+++ gate/models/backbones/timm.py
@@ -11,12 +11,48 @@ from timm.data.transforms_factory import create_transform
 from transformers import CLIPModel, CLIPProcessor
 from transformers.models.clip.modeling_clip import CLIPOutput
 
+from dataclasses import dataclass
+import PIL
+import torch
+import torchvision.transforms as T
+
+
 from gate.models.backbones import (
     Modality,
-    apply_preprocessing_transforms,
     image_dim_reshape,
 )
 from gate.models.core import reinit
+
+single_to_three_channel = T.Lambda(lambda x: x.repeat(3, 1, 1))
+
+
+def apply_preprocessing_transforms(transforms, x, modality=Modality.image):
+    input_shape = None
+    is_5d_tensor = False
+    if isinstance(x, PIL.Image.Image) and modality == Modality.image:
+        x = T.ToTensor()(x)
+        if x.shape[0] == 1:
+            x = single_to_three_channel(x)
+
+    if isinstance(x, torch.Tensor) and modality == Modality.image:
+        input_shape = x.shape
+        x = image_dim_reshape(x)
+        is_5d_tensor = len(x.shape) == 5
+
+    if transforms is not None:
+        x = transforms(x)
+
+    if (
+        input_shape is not None
+        and isinstance(x, torch.Tensor)
+        and is_5d_tensor
+    ):
+        x = x.view(input_shape[0], input_shape[1], *x.shape[1:])
+
+    if isinstance(x, torch.Tensor):
+        x = T.ToPILImage()(x)
+
+    return x
 
 
 class TimmModel(nn.Module):
