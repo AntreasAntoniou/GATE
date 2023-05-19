efc4ed9342f3a12161729300b6feb5bb71aa31b9
Antreas Antoniou <iam@antreas.io>
2023-05-19 17:41:30 +0000

    update

--- gate/models/backbones/__init__.py
+++ gate/models/backbones/__init__.py
@@ -24,6 +24,7 @@ single_to_three_channel = T.Lambda(lambda x: x.repeat(3, 1, 1))
 
 def apply_preprocessing_transforms(transforms, x, modality=Modality.image):
     input_shape = None
+    is_5d_tensor = False
     if isinstance(x, PIL.Image.Image) and modality == Modality.image:
         x = T.ToTensor()(x)
         if x.shape[0] == 1:
@@ -32,11 +33,16 @@ def apply_preprocessing_transforms(transforms, x, modality=Modality.image):
     if isinstance(x, torch.Tensor) and modality == Modality.image:
         input_shape = x.shape
         x = image_dim_reshape(x)
+        is_5d_tensor = len(x.shape) == 5
 
     if transforms is not None:
         x = transforms(x)
 
-    if input_shape is not None and isinstance(x, torch.Tensor):
+    if (
+        input_shape is not None
+        and isinstance(x, torch.Tensor)
+        and is_5d_tensor
+    ):
         x = x.view(input_shape[0], input_shape[1], *x.shape[1:])
 
     return x
