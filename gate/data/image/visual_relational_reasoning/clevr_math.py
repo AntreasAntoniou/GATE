d5309897743f57018a1cdbc4ca1302173b49af8f
Antreas Antoniou <iam@antreas.io>
2023-05-21 22:12:16 +0000

    update

--- gate/data/image/visual_relational_reasoning/clevr_math.py
+++ gate/data/image/visual_relational_reasoning/clevr_math.py
@@ -21,8 +21,9 @@ def transform_wrapper(inputs: Dict, target_size=224):
     # print(list(inputs.keys()))
     # print(inputs["label"])
     return {
-        # "image": T.Resize(size=(target_size, target_size))(inputs["image"]),
-        "image": inputs["image"],
+        "image": T.Resize(size=(target_size, target_size))(
+            inputs["image"].convert("RGB")
+        ),
         "text": inputs["question"],
         "labels": torch.tensor(int(inputs["label"])).long(),
     }
