5357f418604e0b3912055f14602071b333c2d647
Antreas Antoniou <iam@antreas.io>
2023-05-22 00:37:26 +0000

    update

--- gate/models/task_specific_models/visual_relational_reasoning/tali.py
+++ gate/models/task_specific_models/visual_relational_reasoning/tali.py
@@ -141,3 +141,100 @@ def build_gate_model(
     return ModelAndTransform(
         model=gate_model, transform=model_and_transform.transform
     )
+
+
+colour_dict = {
+    "blue": 0,
+    "brown": 1,
+    "cyan": 2,
+    "gray": 3,
+    "green": 4,
+    "purple": 5,
+    "red": 6,
+    "yellow": 7,
+}
+
+shape_dict = {
+    "cube": 0,
+    "cylinder": 1,
+    "sphere": 2,
+}
+
+count_dict = {
+    "0": 0,
+    "1": 1,
+    "10": 2,
+    "2": 3,
+    "3": 4,
+    "4": 5,
+    "5": 6,
+    "6": 7,
+    "7": 8,
+    "8": 9,
+    "9": 10,
+}
+
+size_dict = {
+    "large": 0,
+    "small": 1,
+}
+
+yes_no_dict = {"no": 0, "yes": 1}
+
+material_dict = {"metal": 0, "rubber": 1}
+
+
+@configurable(
+    group="model",
+    name="tali-relational-reasoning-multi-task",
+)
+def build_gate_model(
+    clip_model_name: str = "openai/clip-vit-base-patch16",
+    whisper_model_name: str = "openai/whisper-small",
+    model_repo_path: str = "Antreas/tali-2-tali_omni_base_patch16_224-wit_tali_image_text_audio_video_dataset-2306",
+    checkpoint_identifier: str = "latest",
+    pretrained: bool = True,
+    modality_a_identifier: str = "image",
+    modality_b_identifier: str = "text",
+    num_projection_features: int = 512,
+    dropout_fusion_prob: float = 0.0,
+    num_classes: Dict = {
+        "colour": len(colour_dict),
+        "shape": len(shape_dict),
+        "count": len(count_dict),
+        "size": len(size_dict),
+        "yes_no": len(yes_no_dict),
+        "material": len(material_dict),
+    },
+):
+    model_and_transform = build_model(
+        clip_model_name=clip_model_name,
+        whisper_model_name=whisper_model_name,
+        model_repo_path=model_repo_path,
+        checkpoint_identifier=checkpoint_identifier,
+        pretrained=pretrained,
+        modality_a_identifier=modality_a_identifier,
+        modality_b_identifier=modality_b_identifier,
+        num_projection_features=num_projection_features,
+        dropout_fusion_prob=dropout_fusion_prob,
+        num_classes=num_classes,
+    )
+
+    model_modality_config_image_classification = TargetModalityConfig(
+        image_text=[SourceModalityConfig(image=True, text=True)]
+    )
+
+    model_key_remapper_dict_config = {
+        "image": "image",
+        "text": "text",
+    }
+
+    gate_model = GATEModel(
+        config=model_modality_config_image_classification,
+        model=model_and_transform.model,
+        key_remapper_dict=model_key_remapper_dict_config,
+    )
+
+    return ModelAndTransform(
+        model=gate_model, transform=model_and_transform.transform
+    )
