1a218bc5659a651b01160dfa809ce35fd1e3d98e
Antreas Antoniou <iam@antreas.io>
2023-05-22 00:02:30 +0000

    update

--- gate/models/task_adapters/visual_relational_reasoning_classification.py
+++ gate/models/task_adapters/visual_relational_reasoning_classification.py
@@ -207,14 +207,14 @@ class DuoModalFusionModel(BaseModule):
                 temp_features = features[answer_specific_idx]
                 temp_logits = self.classifier[answer](temp_features)
                 temp_labels = labels[answer_specific_idx]
-                print(
-                    f"temp_labels {temp_labels.shape}, temp_logits {temp_logits.shape}"
-                )
+                # print(
+                #     f"temp_labels {temp_labels.shape}, temp_logits {temp_logits.shape}"
+                # )
                 output_dict[f"loss_{answer}"] = F.cross_entropy(
                     temp_logits, temp_labels
                 )
                 output_dict[f"accuracy_top_1_{answer}"] = accuracy_top_k(
-                    temp_logits, temp_labels, top_k=1
+                    temp_logits, temp_labels, k=1
                 )
                 output_dict[f"logits_{answer}"] = temp_logits
 
