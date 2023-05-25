8f4ec77336cc43f6fe312718335b4b054b6f06c6
Antreas Antoniou <iam@antreas.io>
2023-05-25 05:06:30 +0000

    update

--- playgrounds/test_experiment_manager.py
+++ playgrounds/test_experiment_manager.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     commands = {}
     for i in range(100):
         commands[f"test_{i}"] = f"python {__file__} test_gpu --gpu_id {i % 10}"
-    er = ExperimentRunner(num_gpus=10, log_dir="./logs")
+    er = ExperimentRunner(num_gpus=2, log_dir="./logs")
     er.run_experiments(commands)
