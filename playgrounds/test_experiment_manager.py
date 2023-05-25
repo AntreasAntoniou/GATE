b49c3d8f259af5b02f171563ad957dab2d501b96
Antreas Antoniou <iam@antreas.io>
2023-05-25 05:17:47 +0000

    update

--- playgrounds/test_experiment_manager.py
+++ playgrounds/test_experiment_manager.py
@@ -4,6 +4,6 @@ from playgrounds.experiment_manager import ExperimentRunner
 if __name__ == "__main__":
     commands = {}
     for i in range(100):
-        commands[f"test_{i}"] = f"python {__file__} test_gpu --gpu_id {i % 10}"
-    er = ExperimentRunner(num_gpus=2, log_dir="./logs")
+        commands[f"test_{i}"] = f"python {__file__} test_gpu --gpu_id {i % 2}"
+    er = ExperimentRunner(num_gpus=2, log_dir="/logs")
     er.run_experiments(commands)
