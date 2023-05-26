11f8bd2755d3a1e2d46309d18efbf0897f4da32c
Antreas Antoniou <iam@antreas.io>
2023-05-26 23:10:48 +0000

    updates

--- gate/data/image/classification/food101.py
+++ gate/data/image/classification/food101.py
@@ -71,24 +71,18 @@ def build_gate_food_101_dataset(
     train_set = GATEDataset(
         dataset=build_food101_dataset("train", data_dir=data_dir),
         infinite_sampling=True,
-        task=ClassificationTask(),
-        key_remapper_dict={"pixel_values": "image"},
         transforms=transforms,
     )
 
     val_set = GATEDataset(
         dataset=build_food101_dataset("val", data_dir=data_dir),
         infinite_sampling=False,
-        task=ClassificationTask(),
-        key_remapper_dict={"pixel_values": "image"},
         transforms=transforms,
     )
 
     test_set = GATEDataset(
         dataset=build_food101_dataset("test", data_dir=data_dir),
         infinite_sampling=False,
-        task=ClassificationTask(),
-        key_remapper_dict={"pixel_values": "image"},
         transforms=transforms,
     )
 
