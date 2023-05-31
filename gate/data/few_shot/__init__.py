71667eb5ed051cff4d6099e937b584a918e6b03f
Antreas Antoniou <iam@antreas.io>
2023-05-31 20:52:38 +0000

    remove minor error causing bugs

--- gate/data/few_shot/__init__.py
+++ gate/data/few_shot/__init__.py
@@ -2,18 +2,3 @@ def bytes_to_string(x):
     return (
         x.decode("utf-8").lower() if isinstance(x, bytes) else str(x).lower()
     )
-
-
-from .aircraft import AircraftFewShotClassificationDataset
-from .cifarfs import CIFARFewShotClassificationDataset
-from .cubirds200 import CUB200FewShotClassificationDataset
-from .describable_textures import (
-    DescribableTexturesFewShotClassificationDataset,
-)
-from .fc100 import FC100FewShotClassificationDataset
-from .fungi import FungiFewShotClassificationDataset
-from .mini_imagenet import MiniImageNetFewShotClassificationDataset
-from .omniglot import OmniglotFewShotClassificationDataset
-from .quickdraw import QuickDrawFewShotClassificationDataset
-from .tiered_imagenet import TieredImageNetFewShotClassificationDataset
-from .vggflowers import VGGFlowersFewShotClassificationDataset
