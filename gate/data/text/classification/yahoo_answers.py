d5b3cae252322b2f2d62aef9370408e8f4758bb8
Fady Rezk <24905782+FadyRezkGhattas@users.noreply.github.com>
2023-06-02 16:28:25 +0100

    yahoo answers loader fix

--- gate/data/text/classification/yahoo_answers.py
+++ gate/data/text/classification/yahoo_answers.py
@@ -1,18 +1,37 @@
 # yahoo_answers.py
-import multiprocessing as mp
-from typing import Any, Optional
+import os
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
 
+import torch
 import numpy as np
 from datasets import load_dataset
-
 from gate.boilerplate.decorators import configurable
 from gate.config.variables import DATASET_DIR
 from gate.data.core import GATEDataset
 from gate.data.tasks.text_classification import YahooAnswersTask
 
+def dataset_format_transform(sample: Dict) -> Dict:
+    input_dict = {}
+    input_dict["text"] = sample["question_title"] + sample['question_content']
+    input_dict["labels"] = sample["topic"]
+    return input_dict
+
+class_description = {
+    0: 'Society & Culture',
+    1: 'Science & Mathematics',
+    2: 'Health',
+    3: 'Education & Reference',
+    4: 'Computers & Internet',
+    5: 'Sports',
+    6: 'Business & Finance',
+    7: 'Entertainment & Music',
+    8: 'Family & Relationships',
+    9: 'Politics & Government'
+}
 
 def build_yahoo_answers_dataset(
-    set_name: str, data_dir: Optional[str] = None
+    data_dir: str, set_name: str
 ) -> dict:
     """
     Build a Yahoo Answers dataset using the Hugging Face datasets library.
@@ -46,47 +65,48 @@ def build_yahoo_answers_dataset(
 
 
 @configurable(
-    group="dataset", name="yaho_answers", defaults=dict(data_dir=DATASET_DIR)
+    group="dataset", name="yahoo_answers", defaults=dict(data_dir=DATASET_DIR)
 )
 def build_gate_yahoo_answers_dataset(
     data_dir: Optional[str] = None,
     transforms: Optional[Any] = None,
+    num_classes=10
 ) -> dict:
     train_set = GATEDataset(
-        dataset=build_yahoo_answers_dataset("train", data_dir=data_dir),
+        dataset=build_yahoo_answers_dataset(data_dir, "train"),
         infinite_sampling=True,
-        task=YahooAnswersTask(),
-        key_remapper_dict={"label": "labels"},
-        transforms=transforms,
+        transforms=[dataset_format_transform, transforms],
     )
 
     val_set = GATEDataset(
-        dataset=build_yahoo_answers_dataset("val", data_dir=data_dir),
+        dataset=build_yahoo_answers_dataset(data_dir, "val"),
         infinite_sampling=False,
-        task=YahooAnswersTask(),
-        key_remapper_dict={"label": "labels"},
-        transforms=transforms,
+        transforms=[dataset_format_transform, transforms],
     )
 
     test_set = GATEDataset(
-        dataset=build_yahoo_answers_dataset("test", data_dir=data_dir),
+        dataset=build_yahoo_answers_dataset(data_dir, "test"),
         infinite_sampling=False,
-        task=YahooAnswersTask(),
-        key_remapper_dict={"label": "label"},
-        transforms=transforms,
+        transforms=[dataset_format_transform, transforms],
     )
 
     dataset_dict = {"train": train_set, "val": val_set, "test": test_set}
     return dataset_dict
 
+@dataclass
+class DefaultHyperparameters:
+    train_batch_size: int = 32
+    eval_batch_size: int = 128
+    num_classes: int = 10
+
 
 # For debugging and testing purposes
 if __name__ == "__main__":
     print("BEFORE TRANSFORMING THE DATASET")
-    train_data = build_yahoo_answers_dataset("train")
+    train_data = build_yahoo_answers_dataset(os.environ["DATASET_DIR"], "train")
     print(train_data[0])
     print("GATE DATASET")
-    data = build_gate_yahoo_answers_dataset()
+    data = build_gate_yahoo_answers_dataset(os.environ["DATASET_DIR"])
     print(data["train"][0])
     print(data["val"][0])
     print(data["test"][0])
--- tests/data/text/classification/test_yahoo_answers_classification.py
+++ tests/data/text/classification/test_yahoo_answers_classification.py
@@ -12,24 +12,24 @@ def test_build_yahoo_answers_dataset():
     # Test if the function returns the correct dataset split
 
     train_set = build_yahoo_answers_dataset(
-        "train", data_dir=os.environ.get("TEST_DIR")
+        os.environ.get("DATASET_DIR"), "train"
     )
     assert train_set is not None, "Train set should not be None"
 
     val_set = build_yahoo_answers_dataset(
-        "val", data_dir=os.environ.get("TEST_DIR")
+        os.environ.get("DATASET_DIR"), "val"
     )
     assert val_set is not None, "Validation set should not be None"
 
     test_set = build_yahoo_answers_dataset(
-        "test", data_dir=os.environ.get("TEST_DIR")
+        os.environ.get("DATASET_DIR"), "test"
     )
     assert test_set is not None, "Test set should not be None"
 
     # Test if the function raises an error when an invalid set_name is given
     with pytest.raises(KeyError):
         build_yahoo_answers_dataset(
-            "invalid_set_name", data_dir=os.environ.get("TEST_DIR")
+            os.environ.get("DATASET_DIR"), "invalid_set_name"
         )
 
 
