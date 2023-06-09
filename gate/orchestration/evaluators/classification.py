adf7c92e2bcb3f169536d0f9d54166f70963fc53
Antreas Antoniou <iam@antreas.io>
2023-06-09 03:25:37 +0000

    updates

--- gate/orchestration/evaluators/classification.py
+++ gate/orchestration/evaluators/classification.py
@@ -42,9 +42,9 @@ class StepOutput:
 
 class ClassificationEvaluator(Evaluator):
     def step(self, model, batch, global_step, accelerator: Accelerator):
-        output_dict = model.forward(batch)[self.target_modality][
-            self.source_modality
-        ]
+        output_dict = model.forward(batch)
+        print(output_dict)
+        output_dict = output_dict[self.target_modality][self.source_modality]
 
         loss = output_dict["loss"]
 
