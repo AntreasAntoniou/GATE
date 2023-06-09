806e12ed282a085eb47b50f577cae981d2473328
Antreas Antoniou <iam@antreas.io>
2023-06-09 17:36:26 +0000

    updates

--- gate/models/task_adapters/semantic_segmentation.py
+++ gate/models/task_adapters/semantic_segmentation.py
@@ -278,7 +278,7 @@ class SegmentationViT(nn.Module):
                     in_channels=num_classes,
                     out_channels=num_classes,
                 )
-                for _ in range(2)
+                for _ in range(3)
             ]
         )
         self.additional_projection = None
