a4929bcaed66333423d63313e63567b4081258be
Antreas Antoniou <iam@antreas.io>
2023-06-09 00:26:41 +0000

    updates

--- gate/metrics/segmentation.py
+++ gate/metrics/segmentation.py
@@ -54,8 +54,8 @@ def loss_adapter(
 def normalized_surface_dice_loss(
     logits, labels, label_dim, num_classes, class_thresholds: list = [0.5]
 ):
-    # logits = logits.permute(0, 2, 3, 1).reshape(-1, num_classes)
-    # labels = labels.permute(0, 2, 3, 1).reshape(-1)
+    logits = logits.permute(0, 2, 3, 1).reshape(-1, num_classes)
+    labels = labels.permute(0, 2, 3, 1).reshape(-1)
     return loss_adapter(
         loss_fn=monai.metrics.compute_surface_dice,
         logits=logits,
@@ -167,3 +167,34 @@ def diff_sigmoid_focal_loss(
         loss = alpha_t * loss
 
     return loss.mean()
+
+
+SMOOTH = 1e-6
+
+
+def fast_miou(logits: torch.Tensor, labels: torch.Tensor):
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    logits = logits.argmax(dim=1).squeeze(
+        1
+    )  # BATCH x 1 x H x W => BATCH x H x W
+
+    intersection = (
+        (logits & labels).float().sum((1, 2))
+    )  # Will be zero if Truth=0 or Prediction=0
+    union = (
+        (logits | labels).float().sum((1, 2))
+    )  # Will be zzero if both are 0
+
+    iou = (intersection + SMOOTH) / (
+        union + SMOOTH
+    )  # We smooth our devision to avoid 0/0
+
+    thresholded = (
+        torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
+    )  # This is equal to comparing with thresolds
+
+    return (
+        thresholded.mean()
+    )  # Or thresholded.mean() if you are interested in average across the batch
