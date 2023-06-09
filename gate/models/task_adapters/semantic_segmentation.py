024b1eed8bfccf38f5b9e29bc646b806584990fc
Antreas Antoniou <iam@antreas.io>
2023-06-09 14:12:11 +0000

    updates

--- gate/models/task_adapters/semantic_segmentation.py
+++ gate/models/task_adapters/semantic_segmentation.py
@@ -162,7 +162,7 @@ def optimization_loss(logits, labels):
     dice_loss = diff_dice_loss(logits, labels)
     focal_loss = diff_sigmoid_focal_loss(logits, labels)
 
-    loss = dice_loss + focal_loss  # + cross_entropy_loss
+    loss = dice_loss + focal_loss + cross_entropy_loss
 
     return {
         "loss": loss,
