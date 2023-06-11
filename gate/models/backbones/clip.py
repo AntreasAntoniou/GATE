43c1d87201c2ae7ca9d8702a3bd83cc607c24e3c
Antreas Antoniou <iam@antreas.io>
2023-06-11 01:27:08 +0000

    updates

--- gate/models/backbones/clip.py
+++ gate/models/backbones/clip.py
@@ -99,7 +99,7 @@ class CLIPAdapter(nn.Module):
     def get_transforms(self):
         def image_transforms(x):
             return self.preprocessor(
-                images=x, return_tensors="pt"
+                images=x, do_center_crop=False, return_tensors="pt"
             ).pixel_values.squeeze(0)
 
         def text_transforms(x):
