0da7f6ea39155f2bf087d7420067783fbc8f4604
Antreas Antoniou <iam@antreas.io>
2023-06-12 20:15:58 +0000

    updates

--- gate/metrics/segmentation.py
+++ gate/metrics/segmentation.py
@@ -401,3 +401,210 @@ def fast_miou_numpy(
         ignore_index=ignore_index,
         nan_to_num=1e-8,
     )
+
+
+class MixSoftmaxCrossEntropyLoss(torch.nn.CrossEntropyLoss):
+    def __init__(self, aux=True, aux_weight=0.2, ignore_index=-1, **kwargs):
+        super(MixSoftmaxCrossEntropyLoss, self).__init__(
+            ignore_index=ignore_index
+        )
+        self.aux = aux
+        self.aux_weight = aux_weight
+
+    def _aux_forward(self, *inputs, **kwargs):
+        *preds, target = tuple(inputs)
+
+        loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
+            preds[0], target
+        )
+        for i in range(1, len(preds)):
+            aux_loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
+                preds[i], target
+            )
+            loss += self.aux_weight * aux_loss
+        return loss
+
+    def _multiple_forward(self, *inputs):
+        *preds, target = tuple(inputs)
+        loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
+            preds[0], target
+        )
+        for i in range(1, len(preds)):
+            loss += super(MixSoftmaxCrossEntropyLoss, self).forward(
+                preds[i], target
+            )
+        return loss
+
+    def forward(self, *inputs, **kwargs):
+        preds, target = tuple(inputs)
+        inputs = tuple(list(preds) + [target])
+        if self.aux:
+            return dict(loss=self._aux_forward(*inputs))
+        elif len(preds) > 1:
+            return dict(loss=self._multiple_forward(*inputs))
+        else:
+            return dict(
+                loss=super(MixSoftmaxCrossEntropyLoss, self).forward(*inputs)
+            )
+
+
+class FocalLoss(torch.nn.Module):
+    def __init__(
+        self,
+        alpha=0.5,
+        gamma=2,
+        weight=None,
+        aux=True,
+        aux_weight=0.2,
+        ignore_index=-1,
+        size_average=True,
+    ):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.weight = weight
+        self.ignore_index = ignore_index
+        self.aux = aux
+        self.aux_weight = aux_weight
+        self.size_average = size_average
+        self.ce_fn = torch.nn.CrossEntropyLoss(
+            weight=self.weight, ignore_index=self.ignore_index
+        )
+
+    def _aux_forward(self, *inputs, **kwargs):
+        *preds, target = tuple(inputs)
+
+        loss = self._base_forward(preds[0], target)
+        for i in range(1, len(preds)):
+            aux_loss = self._base_forward(preds[i], target)
+            loss += self.aux_weight * aux_loss
+        return loss
+
+    def _base_forward(self, output, target):
+        if output.dim() > 2:
+            output = output.contiguous().view(
+                output.size(0), output.size(1), -1
+            )
+            output = output.transpose(1, 2)
+            output = output.contiguous().view(-1, output.size(2)).squeeze()
+        if target.dim() == 4:
+            target = target.contiguous().view(
+                target.size(0), target.size(1), -1
+            )
+            target = target.transpose(1, 2)
+            target = target.contiguous().view(-1, target.size(2)).squeeze()
+        elif target.dim() == 3:
+            target = target.view(-1)
+        else:
+            target = target.view(-1, 1)
+
+        logpt = self.ce_fn(output, target)
+        pt = torch.exp(-logpt)
+        loss = ((1 - pt) ** self.gamma) * self.alpha * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+    def forward(self, logits, labels):
+        inputs = tuple(list(logits) + [labels])
+        return dict(loss=self._aux_forward(*inputs))
+
+
+class BinaryDiceLoss(torch.nn.Module):
+    """Dice loss of binary class
+    Args:
+        smooth: A float number to smooth loss, and avoid NaN error, default: 1
+        p: Denominator value: \sum{x^p} + \sum{y^p}, default: 2
+        predict: A tensor of shape [N, *]
+        target: A tensor of shape same with predict
+        reduction: Reduction method to apply, return mean over batch if 'mean',
+            return sum if 'sum', return a tensor of shape [N,] if 'none'
+    Returns:
+        Loss tensor according to arg reduction
+    Raise:
+        Exception if unexpected reduction
+    """
+
+    def __init__(self, smooth=1, p=2, reduction="mean"):
+        super(BinaryDiceLoss, self).__init__()
+        self.smooth = smooth
+        self.p = p
+        self.reduction = reduction
+
+    def forward(self, predict, target, valid_mask):
+        assert (
+            predict.shape[0] == target.shape[0]
+        ), "predict & target batch size don't match"
+        predict = predict.contiguous().view(predict.shape[0], -1)
+        target = target.contiguous().view(target.shape[0], -1)
+        valid_mask = valid_mask.contiguous().view(valid_mask.shape[0], -1)
+
+        num = (
+            torch.sum(torch.mul(predict, target) * valid_mask, dim=1) * 2
+            + self.smooth
+        )
+        den = (
+            torch.sum(
+                (predict.pow(self.p) + target.pow(self.p)) * valid_mask, dim=1
+            )
+            + self.smooth
+        )
+
+        loss = 1 - num / den
+
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+        elif self.reduction == "none":
+            return loss
+        else:
+            raise Exception("Unexpected reduction {}".format(self.reduction))
+
+
+class DiceLoss(torch.nn.Module):
+    """Dice loss, need one hot encode input"""
+
+    def __init__(
+        self, weight=None, aux=True, aux_weight=0.4, ignore_index=-1, **kwargs
+    ):
+        super(DiceLoss, self).__init__()
+        self.kwargs = kwargs
+        self.weight = weight
+        self.ignore_index = ignore_index
+        self.aux = aux
+        self.aux_weight = aux_weight
+
+    def _base_forward(self, predict, target, valid_mask):
+        dice = BinaryDiceLoss(**self.kwargs)
+        total_loss = 0
+        predict = F.softmax(predict, dim=1)
+
+        for i in range(target.shape[-1]):
+            if i != self.ignore_index:
+                dice_loss = dice(predict[:, i], target[..., i], valid_mask)
+                if self.weight is not None:
+                    assert (
+                        self.weight.shape[0] == target.shape[1]
+                    ), "Expect weight shape [{}], get[{}]".format(
+                        target.shape[1], self.weight.shape[0]
+                    )
+                    dice_loss *= self.weights[i]
+                total_loss += dice_loss
+
+        return total_loss / target.shape[-1]
+
+    def _aux_forward(self, *inputs, **kwargs):
+        *preds, target = tuple(inputs)
+        valid_mask = (target != self.ignore_index).long()
+        target_one_hot = F.one_hot(torch.clamp_min(target, 0))
+        loss = self._base_forward(preds[0], target_one_hot, valid_mask)
+        for i in range(1, len(preds)):
+            aux_loss = self._base_forward(preds[i], target_one_hot, valid_mask)
+            loss += self.aux_weight * aux_loss
+        return loss
+
+    def forward(self, logits, labels):
+        inputs = tuple(list(logits) + [labels])
+        return dict(loss=self._aux_forward(*inputs))
