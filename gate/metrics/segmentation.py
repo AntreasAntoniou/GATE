97792af54f3f508ea03a742d72249a2c51113d0f
Antreas Antoniou <iam@antreas.io>
2023-06-12 21:14:39 +0000

    updates

--- gate/metrics/segmentation.py
+++ gate/metrics/segmentation.py
@@ -421,51 +421,6 @@ def one_hot(labels: torch.Tensor, num_classes: int):
     return one_hot_vectors
 
 
-class MixSoftmaxCrossEntropyLoss(torch.nn.CrossEntropyLoss):
-    def __init__(self, aux=True, aux_weight=0.2, ignore_index=-1, **kwargs):
-        super(MixSoftmaxCrossEntropyLoss, self).__init__(
-            ignore_index=ignore_index
-        )
-        self.aux = aux
-        self.aux_weight = aux_weight
-
-    def _aux_forward(self, *inputs, **kwargs):
-        *preds, target = tuple(inputs)
-
-        loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
-            preds[0], target
-        )
-        for i in range(1, len(preds)):
-            aux_loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
-                preds[i], target
-            )
-            loss += self.aux_weight * aux_loss
-        return loss
-
-    def _multiple_forward(self, *inputs):
-        *preds, target = tuple(inputs)
-        loss = super(MixSoftmaxCrossEntropyLoss, self).forward(
-            preds[0], target
-        )
-        for i in range(1, len(preds)):
-            loss += super(MixSoftmaxCrossEntropyLoss, self).forward(
-                preds[i], target
-            )
-        return loss
-
-    def forward(self, *inputs, **kwargs):
-        preds, target = tuple(inputs)
-        inputs = tuple(list(preds) + [target])
-        if self.aux:
-            return dict(loss=self._aux_forward(*inputs))
-        elif len(preds) > 1:
-            return dict(loss=self._multiple_forward(*inputs))
-        else:
-            return dict(
-                loss=super(MixSoftmaxCrossEntropyLoss, self).forward(*inputs)
-            )
-
-
 class FocalLoss(torch.nn.Module):
     def __init__(
         self,
@@ -526,12 +481,10 @@ class FocalLoss(torch.nn.Module):
 
     def forward(self, logits, labels):
         logits = logits.permute(0, 2, 3, 1).reshape(-1, logits.shape[1])
-        labels = (
-            labels.permute(0, 2, 3, 1).reshape(-1, labels.shape[1]).squeeze(1)
-        )
-        labels = one_hot(labels, logits.shape[1])
+        labels = labels.permute(0, 2, 3, 1).reshape(-1).squeeze(1)
+        # labels = one_hot(labels, logits.shape[1])
         inputs = tuple(list(logits) + [labels])
-        return dict(loss=self._aux_forward(*inputs))
+        return self._aux_forward(*inputs)
 
 
 class BinaryDiceLoss(torch.nn.Module):
