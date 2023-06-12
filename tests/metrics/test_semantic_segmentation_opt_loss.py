ca460946af7af98df2e5ee2b7572a3b90410e627
Antreas Antoniou <iam@antreas.io>
2023-06-12 21:43:12 +0000

    updates

---
+++ tests/metrics/test_semantic_segmentation_opt_loss.py
@@ -0,0 +1,44 @@
+import torch
+import pytest
+
+from gate.metrics.segmentation import DiceLoss, FocalLoss
+
+# Replace `your_module` with the actual module name containing the classes
+
+
+@pytest.fixture
+def logits():
+    return torch.randn(4, 3, 32, 32)
+
+
+@pytest.fixture
+def labels():
+    return torch.randint(0, 3, (4, 1, 32, 32))
+
+
+def test_dice_loss_shape(logits, labels):
+    dice_loss = DiceLoss()
+    loss = dice_loss(logits, labels.squeeze(1))
+    assert loss.shape == torch.Size([]), "DiceLoss should return a scalar loss"
+
+
+def test_dice_loss_value(logits, labels):
+    dice_loss = DiceLoss()
+    loss = dice_loss(logits, labels.squeeze(1))
+    assert (
+        0 <= loss.item() <= 1
+    ), "DiceLoss should return a value between 0 and 1"
+
+
+def test_focal_loss_shape(logits, labels):
+    focal_loss = FocalLoss()
+    loss = focal_loss(logits, labels.squeeze(1))
+    assert loss.shape == torch.Size(
+        []
+    ), "FocalLoss should return a scalar loss"
+
+
+def test_focal_loss_value(logits, labels):
+    focal_loss = FocalLoss()
+    loss = focal_loss(logits, labels.squeeze(1))
+    assert 0 <= loss.item(), "FocalLoss should return a non-negative value"
