78d5edf9052b7b0265c3619599c11a1408e023f6
Antreas Antoniou <iam@antreas.io>
2023-06-13 01:24:40 +0000

    updates

--- gate/metrics/segmentation.py
+++ gate/metrics/segmentation.py
@@ -474,7 +474,8 @@ class DiceLoss(nn.Module):
     def forward(self, logits, labels):
         b, c, h, w = logits.shape
         logits = F.softmax(logits, dim=1)
-        labels = labels.view(-1)
+        labels = labels.squeeze(1)
+        # print(f"labels.shape: {labels.shape}")
 
         labels_one_hot = torch.zeros_like(logits)
         labels_one_hot.scatter_(1, labels.unsqueeze(1), 1)
