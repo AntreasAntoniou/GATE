2e059844f04fc327372b4adce9c9237bf5b729fd
Antreas Antoniou <iam@antreas.io>
2023-06-13 22:13:37 +0000

    updates

--- gate/models/task_adapters/duo_modal_zero_shot_classification.py
+++ gate/models/task_adapters/duo_modal_zero_shot_classification.py
@@ -1,15 +1,21 @@
+import logging
+import time
 from typing import Dict, List, Optional
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from tqdm.auto import tqdm
+from gate.boilerplate.utils import get_logger
+
 
 from gate.models.task_adapters import BaseModule
 from gate.models.task_adapters.utils import (
     compute_zero_shot_loss_and_metrics,
     get_similarities,
 )
+
+logger = get_logger(__name__)
 
 
 class DuoModalZeroShotModel(BaseModule):
@@ -79,6 +85,7 @@ class DuoModalZeroShotModel(BaseModule):
 
         is_irregular_shape = False
         # print(f"image.shape: {image.shape}, text.shape: {text.shape}")
+        start_time = time.time()
         if len(image.shape) == 5:
             image = image.view(-1, *image.shape[2:])
             is_irregular_shape = True
@@ -96,7 +103,8 @@ class DuoModalZeroShotModel(BaseModule):
                 modality_b_features = self.modality_b_model(image=image)[
                     self.modality_b_identifier
                 ][self.head_identifier]
-
+        logger.info(f"Time taken for image: {time.time() - start_time}")
+        start_time = time.time()
         if text is not None:
             if self.modality_a_identifier == "text":
                 modality_a_features = self.modality_a_model(text=text)[
@@ -106,6 +114,7 @@ class DuoModalZeroShotModel(BaseModule):
                 modality_b_features = self.modality_b_model(text=text)[
                     self.modality_b_identifier
                 ][self.head_identifier]
+        logger.info(f"Time taken for text: {time.time() - start_time}")
         if audio is not None:
             if self.modality_a_identifier == "audio":
                 modality_a_features = self.modality_a_model(audio=audio)[
@@ -126,10 +135,13 @@ class DuoModalZeroShotModel(BaseModule):
                     self.modality_b_identifier
                 ][self.head_identifier]
 
+        start_time = time.time()
         if self.projection_num_features is not None:
             modality_a_features = self.modality_a_linear(modality_a_features)
             modality_b_features = self.modality_b_linear(modality_b_features)
+        logger.info(f"Time taken for projection: {time.time() - start_time}")
 
+        start_time = time.time()
         similarities_dict = get_similarities(
             modality_a_name=self.modality_a_identifier,
             modality_a_features=modality_a_features,
@@ -137,6 +149,8 @@ class DuoModalZeroShotModel(BaseModule):
             modality_b_features=modality_b_features,
             temperature_parameter=self.temperature_parameter,
         )
+        logger.info(f"Time taken for similarities: {time.time() - start_time}")
+
         output_dict = {
             "logits": {
                 "similarities": similarities_dict,
@@ -144,17 +158,19 @@ class DuoModalZeroShotModel(BaseModule):
             },
             "labels": similarities_dict,
         }
-
+        start_time = time.time()
         metrics_dict = self.compute_loss_and_metrics(
             logits=output_dict["logits"]
         )
-
+        logger.info(f"Time taken for metrics: {time.time() - start_time}")
+        start_time = time.time()
         losses_list = [
             value for key, value in metrics_dict.items() if "loss" in key
         ]
         if len(losses_list) > 0:
             loss = torch.mean(torch.stack(losses_list))
             metrics_dict["loss"] = loss
+        logger.info(f"Time taken for loss: {time.time() - start_time}")
 
         return output_dict | metrics_dict
 
