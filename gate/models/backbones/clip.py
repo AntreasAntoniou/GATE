aaf595dfd87a3027bd892fe1bf10630d9cc1b7d1
Antreas Antoniou <iam@antreas.io>
2023-06-14 23:50:50 +0000

    updates

--- gate/models/backbones/clip.py
+++ gate/models/backbones/clip.py
@@ -69,10 +69,10 @@ class CLIPAdapter(nn.Module):
         self.text_num_features = self.clip.text_embed_dim
 
     def modify_expected_image_size(self, image_size: int):
-        config = self.clip.vision_model.config
+        config = self.vision_model.config
         config.image_size = image_size
-        self.clip.vision_model.embeddings.position_embedding = (
-            CLIPVisionEmbeddings(config)
+        self.vision_model.embeddings.position_embedding = CLIPVisionEmbeddings(
+            config
         )
 
     def init_weights(self):
