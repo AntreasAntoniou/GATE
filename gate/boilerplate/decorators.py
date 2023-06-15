8bf531bccc41d79773cb60ecd12acd830a367124
Antreas Antoniou <iam@antreas.io>
2023-06-15 18:27:06 +0000

    updates

--- gate/boilerplate/decorators.py
+++ gate/boilerplate/decorators.py
@@ -132,7 +132,7 @@ class BackgroundLogging(threading.Thread):
                     )
                     continue
                 if "ae_episode" in metric_key:
-                    # print("logging ae episode")
+                    print("logging ae episode")
                     ae_episode = value
                     log_wandb_images(
                         experiment_tracker=self.experiment_tracker,
