64782312a8f50eeb6f8df6542444dc3cc88a9ce3
Antreas Antoniou <iam@antreas.io>
2023-06-16 23:58:44 +0000

    updates

--- gate/models/task_adapters/semantic_segmentation.py
+++ gate/models/task_adapters/semantic_segmentation.py
@@ -28,62 +28,6 @@ from gate.boilerplate.utils import get_logger
 logger = get_logger(__name__)
 
 
-def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
-    """
-    embed_dim: output dimension for each position
-    pos: a list of positions to be encoded: size (M,)
-    out: (M, D)
-    """
-    assert embed_dim % 2 == 0
-    omega = np.arange(embed_dim // 2, dtype=np.float32)
-    omega /= embed_dim / 2.0
-    omega = 1.0 / 10000**omega  # (D/2,)
-
-    pos = pos.reshape(-1)  # (M,)
-    out = np.einsum("m,d->md", pos, omega)  # (M, D/2), outer product
-
-    emb_sin = np.sin(out)  # (M, D/2)
-    emb_cos = np.cos(out)  # (M, D/2)
-
-    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
-    return emb
-
-
-def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
-    assert embed_dim % 2 == 0
-
-    # use half of dimensions to encode grid_h
-    emb_h = get_1d_sincos_pos_embed_from_grid(
-        embed_dim // 2, grid[0]
-    )  # (H*W, D/2)
-    emb_w = get_1d_sincos_pos_embed_from_grid(
-        embed_dim // 2, grid[1]
-    )  # (H*W, D/2)
-
-    emb = np.concatenate([emb_h, emb_w], axis=1)  # (H*W, D)
-    return emb
-
-
-def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
-    """
-    grid_size: int of the grid height and width
-    return:
-    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
-    """
-    grid_h = np.arange(grid_size, dtype=np.float32)
-    grid_w = np.arange(grid_size, dtype=np.float32)
-    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
-    grid = np.stack(grid, axis=0)
-
-    grid = grid.reshape([2, 1, grid_size, grid_size])
-    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
-    if cls_token:
-        pos_embed = np.concatenate(
-            [np.zeros([1, embed_dim]), pos_embed], axis=0
-        )
-    return pos_embed
-
-
 class ResidualUpscaleConvBlock(nn.Module):
     """
     📝 Residual Convolutional Block
@@ -353,53 +297,6 @@ class UpscaleMultiBlock(nn.Module):
         return out
 
 
-class PositionalEncoding(nn.Module):
-    def __init__(
-        self,
-    ):
-        super().__init__()
-        self.positional_encoding = None
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            x: Tensor, shape [batch_size, seq_len, embedding_dim] or [batch_size, channels, height, width]
-        """
-        is_image_based = len(x.shape) == 4
-        if self.positional_encoding is None:
-            if len(x.shape) == 4:
-                # [batch_size, channels, height, width]
-                max_len = x.shape[2] * x.shape[3]
-                d_model = x.shape[1]
-            else:
-                max_len = x.shape[1]
-                d_model = x.shape[2]
-
-            position = torch.arange(max_len).unsqueeze(1).to(x.device)
-            div_term = torch.exp(
-                torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
-            ).to(x.device)
-            pe = torch.zeros(1, max_len, d_model).to(x.device)
-            pe[0, :, 0::2] = torch.sin(position * div_term).to(x.device)
-            pe[0, :, 1::2] = torch.cos(position * div_term).to(x.device)
-
-            if is_image_based:
-                pe = pe.reshape(
-                    1,
-                    x.shape[2],
-                    x.shape[3],
-                    x.shape[1],
-                ).permute(0, 3, 1, 2)
-
-            self.positional_encoding = torch.nn.Parameter(
-                pe, requires_grad=True
-            )
-
-        self.positional_encoding = self.positional_encoding.to(x.device)
-        x = x + self.positional_encoding[: x.shape[0]]
-        return x
-
-
 import math
 
 
@@ -545,6 +442,8 @@ class SegmentationViT(nn.Module):
         decoder_embed_dim: int = 512,
         num_classes: int = 100,
         num_patches: int = 14,
+        background_weight: float = 0.01,
+        background_class: int = 0,
         **kwargs,
     ):
         """
@@ -565,30 +464,40 @@ class SegmentationViT(nn.Module):
         self.encoder = encoder_model
         self.num_patches = num_patches
         self.num_classes = num_classes
-        self.positional_encoding = None
+        self.background_weight = background_weight
+        self.background_class = background_class
 
         self.decoder_embedding_dimension = decoder_embed_dim
 
         self.decoder = None
 
-        self.focal_loss = FocalLoss(alpha=0.25, gamma=2, ignore_index=0)
-        self.dice_loss = DiceLoss(ignore_index=0)
-        self.weighted_bce = WeightedCrossEntropyLoss(
-            ignore_index=-1, reduction="mean"
+        self.focal_loss = FocalLoss(
+            alpha=0.25, gamma=2, ignore_index=self.background_class
         )
+        self.dice_loss = DiceLoss(ignore_index=self.background_class)
+        self.background_focal_loss = FocalLoss()
+        self.background_dice_loss = DiceLoss(alpha=0.25, gamma=2)
+
         self.debug_mode = False
 
     def optimization_loss(self, logits, labels):
         focal_loss = self.focal_loss(logits, labels)
         dice_loss = self.dice_loss(logits, labels)
-        wce_loss = self.weighted_bce(logits, labels)
+        background_focal_loss = self.background_focal_loss(logits, labels)
+        background_dice_loss = self.background_dice_loss(logits, labels)
 
-        loss = focal_loss + dice_loss
+        loss = (
+            focal_loss
+            + dice_loss
+            + self.background_weight
+            * (background_focal_loss + background_dice_loss)
+        )
         return {
             "loss": loss,
             "dice_loss": dice_loss,
             "focal_loss": focal_loss,
-            "wce_loss": wce_loss,
+            "background_focal_loss": background_focal_loss,
+            "background_dice_loss": background_dice_loss,
         }
 
     def compute_loss_and_metrics(
