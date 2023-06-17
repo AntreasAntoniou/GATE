4decd5498238393b99bab019bce87b34ff2e08a9
Antreas Antoniou <iam@antreas.io>
2023-06-17 16:57:52 +0000

    updates

---
+++ tests/metrics/test_miou.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+import pytest
+from mmseg.evaluation.metrics import IoUMetric
+
+
+def test_IoUMetric():
+    # Create an instance of your class
+    metric = IoUMetric(ignore_index=255, iou_metrics=["mIoU"], prefix="test")
+
+    # Create some dummy data
+    data_batch = {}
+
+    metric.dataset_meta = {"classes": [0, 1]}
+
+    data_samples = [
+        {
+            "pred_sem_seg": {"data": torch.tensor([[[0, 1], [1, 0]]])},
+            "gt_sem_seg": {"data": torch.tensor([[[0, 1], [1, 0]]])},
+            "img_path": "dummy_path",
+        }
+    ]
+
+    # Call the process method
+    metric.process(data_batch, data_samples)
+
+    print(metric.results)
+
+    # Call the compute_metrics method
+    metrics = metric.compute_metrics(metric.results)
+
+    print(metrics)
+
+    # Check the metrics
+    assert np.allclose(metrics["aAcc"], 100)
+    assert np.allclose(metrics["mIoU"], 100)
