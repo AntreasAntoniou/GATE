8af3553e34aee6f1392ca3e1381e01a9249698db
Antreas Antoniou <iam@antreas.io>
2023-06-23 02:59:27 +0000

    updates

--- gate/data/image/segmentation/pascal_context.py
+++ gate/data/image/segmentation/pascal_context.py
@@ -1,14 +1,24 @@
 # pascal_context.py
-from typing import Optional
+from typing import Any, Dict, List, Optional, Union
 
 import torch
-from torch.utils.data import Dataset, random_split
+import torchvision.transforms as T
 from torchvision.datasets import VOCSegmentation
+from torch.utils.data import random_split
+
+import numpy as np
+from gate.boilerplate.decorators import configurable
+from gate.config.variables import DATASET_DIR
+from gate.data.core import GATEDataset
+from gate.data.image.segmentation.classes import (
+    pascal_context_classes as CLASSES,
+)
+from gate.data.transforms.segmentation_transforms import DualImageRandomCrop
 
 
-def build_pascal_context_dataset(
+def build_dataset(
     set_name: str, data_dir: Optional[str] = None, download: bool = False
-) -> Dataset:
+):
     """
     Build the Pascal Context dataset.
 
@@ -79,3 +89,114 @@ def build_pascal_context_dataset(
         return train_dataset
     elif set_name == "val":
         return val_dataset
+
+
+class DatasetTransforms:
+    def __init__(
+        self,
+        input_size: Union[int, List[int]],
+        target_size: Union[int, List[int]],
+        initial_size: Union[int, List[int]] = 1024,
+        crop_size: Optional[Union[int, List[int]]] = None,
+    ):
+        self.initial_size = (
+            initial_size
+            if isinstance(initial_size, tuple)
+            or isinstance(initial_size, list)
+            else (initial_size, initial_size)
+        )
+        self.input_size = (
+            input_size
+            if isinstance(input_size, tuple) or isinstance(input_size, list)
+            else (input_size, input_size)
+        )
+        self.target_size = (
+            target_size
+            if isinstance(target_size, tuple) or isinstance(target_size, list)
+            else (target_size, target_size)
+        )
+        if crop_size is not None:
+            self.crop_size = (
+                crop_size
+                if isinstance(crop_size, list) or isinstance(crop_size, tuple)
+                else [crop_size, crop_size]
+            )
+            self.crop_transform = DualImageRandomCrop(self.crop_size)
+        else:
+            self.crop_size = None
+
+    def __call__(self, inputs: Dict):
+        image = inputs["image"]
+        image = T.Resize(
+            (self.initial_size[0], self.initial_size[1]),
+            interpolation=T.InterpolationMode.BICUBIC,
+        )(image)
+
+        annotation = inputs["annotation"]
+        annotation = T.Resize(
+            (self.initial_size[0], self.initial_size[1]),
+            interpolation=T.InterpolationMode.BICUBIC,
+        )(annotation)
+
+        if self.crop_size is not None:
+            image, annotation = self.crop_transform(image, annotation)
+
+        image = T.Resize(
+            (self.input_size[0], self.input_size[1]),
+            interpolation=T.InterpolationMode.BICUBIC,
+        )(image)
+
+        annotation = T.Resize(
+            (self.target_size[0], self.target_size[1]),
+            interpolation=T.InterpolationMode.BICUBIC,
+        )(annotation)
+
+        annotation = np.array(annotation)
+        annotation = torch.from_numpy(annotation)
+        annotation = annotation.permute(2, 0, 1)[0].unsqueeze(0)
+
+        return {
+            "image": image,
+            "labels": annotation.long(),
+        }
+
+
+@configurable(
+    group="dataset", name="nyu_depth_v2", defaults=dict(data_dir=DATASET_DIR)
+)
+def build_gate_dataset(
+    data_dir: Optional[str] = None,
+    transforms: Optional[Any] = None,
+    num_classes=150,
+    image_size=512,
+    target_image_size=256,
+) -> dict:
+    train_transforms = DatasetTransforms(
+        image_size, target_image_size, initial_size=1024, crop_size=512
+    )
+    eval_transforms = DatasetTransforms(
+        image_size, target_image_size, initial_size=1024, crop_size=None
+    )
+    train_set = GATEDataset(
+        dataset=build_dataset("train", data_dir=data_dir),
+        infinite_sampling=True,
+        transforms=[train_transforms, transforms],
+        meta_data={"class_names": CLASSES, "num_classes": num_classes},
+    )
+
+    val_set = GATEDataset(
+        dataset=build_dataset("val", data_dir=data_dir),
+        infinite_sampling=False,
+        transforms=[eval_transforms, transforms],
+        meta_data={"class_names": CLASSES, "num_classes": num_classes},
+    )
+
+    test_set = GATEDataset(
+        dataset=build_dataset("test", data_dir=data_dir),
+        infinite_sampling=False,
+        transforms=[eval_transforms, transforms],
+        meta_data={"class_names": CLASSES, "num_classes": num_classes},
+    )
+
+    dataset_dict = {"train": train_set, "val": val_set, "test": test_set}
+    return dataset_dict
--- tests/data/image/segmentation/test_pascal_context_segmentation.py
+++ tests/data/image/segmentation/test_pascal_context_segmentation.py
@@ -1,26 +1,39 @@
+import os
+import pathlib
 import pytest
 
 from gate.data.image.segmentation.pascal_context import (
-    build_pascal_context_dataset,
+    build_dataset,
+    build_gate_dataset,
 )
 
 
-def test_build_pascal_context_dataset():
-    # Test with download=False (Assumes the dataset is already present)
-    train_data = build_pascal_context_dataset("train", download=True)
-    val_data = build_pascal_context_dataset("val", download=True)
-    test_data = build_pascal_context_dataset("test", download=True)
-
-    # Check if the datasets are created without errors
-    assert train_data is not None
-    assert val_data is not None
-    assert test_data is not None
-
-    # Check if the datasets have the correct number of samples
-    assert len(train_data) > 0
-    assert len(val_data) > 0
-    assert len(test_data) > 0
+DATA_DIR = pathlib.Path(os.environ.get("PYTEST_DIR"))
 
 
-if __name__ == "__main__":
-    pytest.main([__file__])
+def test_build_dataset():
+    # Test if the function returns the correct dataset split
+
+    train_set = build_dataset("train", data_dir=DATA_DIR)
+    assert train_set is not None, "Train set should not be None"
+
+    val_set = build_dataset("val", data_dir=DATA_DIR)
+    assert val_set is not None, "Validation set should not be None"
+
+    test_set = build_dataset("test", data_dir=DATA_DIR)
+    assert test_set is not None, "Test set should not be None"
+
+    # Test if the function raises an error when an invalid set_name is given
+    with pytest.raises(KeyError):
+        build_dataset("invalid_set_name", data_dir=DATA_DIR)
+
+
+def test_build_gate_dataset():
+    # Test if the function returns the correct dataset split
+
+    gate_dataset = build_gate_dataset(data_dir=DATA_DIR)
+    assert gate_dataset["train"] is not None, "Train set should not be None"
+    assert gate_dataset["val"] is not None, "Validation set should not be None"
+    assert gate_dataset["test"] is not None, "Test set should not be None"
+
+    print(gate_dataset["train"][0])
