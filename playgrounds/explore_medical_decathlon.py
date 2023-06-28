6bc937ddc4531f89e10dfe404212b31605a86968
AntreasAntoniou <iam@antreas.io>
2023-06-28 04:52:32 +0000

    updates

--- playgrounds/explore_medical_decathlon.py
+++ playgrounds/explore_medical_decathlon.py
@@ -12,6 +12,16 @@ data = datasets.load_dataset(
 keys = list(data.keys())
 print(keys)
 
+# ['image', 'label', 'image_meta_dict', 'label_meta_dict', 'task_name']
+
+
 for item in data["training.task01braintumour"]:
-    print(list(item.keys()))
+    image = item["image"]
+    label = item["label"]
+    image_meta_dict = item["image_meta_dict"]
+    label_meta_dict = item["label_meta_dict"]
+    task_name = item["task_name"]
+    print(
+        f"task_name: {task_name}, image.shape: {image.shape}, label.shape: {label.shape}, image_meta_dict: {image_meta_dict}, label_meta_dict: {label_meta_dict}"
+    )
     break
