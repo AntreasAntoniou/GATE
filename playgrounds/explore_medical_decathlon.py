530b5e941bd520fa08c8e32b8da1761c804a7497
AntreasAntoniou <iam@antreas.io>
2023-06-28 05:06:10 +0000

    updates

--- playgrounds/explore_medical_decathlon.py
+++ playgrounds/explore_medical_decathlon.py
@@ -10,27 +10,28 @@ data = datasets.load_dataset(
     cache_dir=os.environ.get("DATASET_DIR", "/data/tmp/datasets"),
 )
 
-keys = list(data.keys())
-print(keys)
+task_list = list(data.keys())
+print(task_list)
 
 # ['image', 'label', 'image_meta_dict', 'label_meta_dict', 'task_name']
 
 
-for item in data["training.task01braintumour"]:
-    image = (
-        torch.stack([torch.tensor(i) for i in item["image"]])
-        if isinstance(item["image"], list)
-        else item["image"]
-    )
-    label = (
-        torch.stack([torch.tensor(i) for i in item["label"]])
-        if isinstance(item["label"], list)
-        else item["label"]
-    )
-    image_meta_dict = yaml.dump(item["image_meta_dict"])
-    label_meta_dict = yaml.dump(item["label_meta_dict"])
-    task_name = item["task_name"]
-    print(
-        f"task_name: {task_name}, image.shape: {image.shape}, label.shape: {label.shape}, image_meta_dict: {image_meta_dict}, label_meta_dict: {label_meta_dict}"
-    )
-    break
+for task in task_list:
+    for item in data[task]:
+        image = (
+            torch.stack([torch.tensor(i) for i in item["image"]])
+            if isinstance(item["image"], list)
+            else item["image"]
+        )
+        label = (
+            torch.stack([torch.tensor(i) for i in item["label"]])
+            if isinstance(item["label"], list)
+            else item["label"]
+        )
+        image_meta_dict = yaml.dump(item["image_meta_dict"])
+        label_meta_dict = yaml.dump(item["label_meta_dict"])
+        task_name = item["task_name"]
+        print(
+            f"task_name: {task_name}, image.shape: {image.shape}, label.shape: {label.shape}, image_meta_dict: {image_meta_dict}, label_meta_dict: {label_meta_dict}"
+        )
+        break
