c5ee8177f49e5985eb589e83355657bbae45f44f
AntreasAntoniou <iam@antreas.io>
2023-06-28 04:56:05 +0000

    updates

--- playgrounds/explore_medical_decathlon.py
+++ playgrounds/explore_medical_decathlon.py
@@ -16,12 +16,20 @@ print(keys)
 
 
 for item in data["training.task01braintumour"]:
-    image = item["image"]
-    label = item["label"]
+    image = (
+        torch.stack(item["image"])
+        if isinstance(item["image"], list)
+        else item["image"]
+    )
+    label = (
+        torch.stack(item["label"])
+        if isinstance(item["label"], list)
+        else item["label"]
+    )
     image_meta_dict = item["image_meta_dict"]
     label_meta_dict = item["label_meta_dict"]
     task_name = item["task_name"]
     print(
-        f"task_name: {task_name}, image.shape: {image}, label.shape: {label}, image_meta_dict: {image_meta_dict}, label_meta_dict: {label_meta_dict}"
+        f"task_name: {task_name}, image.shape: {image.shape}, label.shape: {label.shape}, image_meta_dict: {image_meta_dict}, label_meta_dict: {label_meta_dict}"
     )
     break
