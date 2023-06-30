30b4ac86b7955c789bc4e97694f8c573e24c72b5
AntreasAntoniou <iam@antreas.io>
2023-06-30 08:52:02 +0000

    updates

--- tests/data/medical/test_segmentation_decathlon.py
+++ tests/data/medical/test_segmentation_decathlon.py
@@ -1,6 +1,8 @@
 import os
 
 import pytest
+import wandb
+from gate.boilerplate.utils import log_wandb_3d_volumes_and_masks
 
 from gate.data.medical.segmentation.medical_decathlon import (
     build_dataset,
@@ -40,3 +42,42 @@ def test_build_gate_dataset():
         assert item["image"] is not None, "Image should not be None"
         assert item["labels"] is not None, "Label should not be None"
         break
+
+
+def test_build_gate_visualize_dataset():
+    gate_dataset = build_gate_dataset(data_dir=os.environ.get("PYTEST_DIR"))
+    assert gate_dataset["train"] is not None, "Train set should not be None"
+    assert gate_dataset["val"] is not None, "Validation set should not be None"
+    assert gate_dataset["test"] is not None, "Test set should not be None"
+
+    for item in gate_dataset["train"]:
+        print(list(item.keys()))
+        assert item["image"] is not None, "Image should not be None"
+        assert item["labels"] is not None, "Label should not be None"
+
+        input_volumes = item["image"].unsqueeze(2)
+        predicted_volumes = item["labels"]
+        label_volumes = item["labels"]
+
+        print(
+            f"Input volumes shape: {input_volumes.shape}, dtype: {input_volumes.dtype}, min: {input_volumes.min()}, max: {input_volumes.max()}, mean: {input_volumes.mean()}, std: {input_volumes.std()}"
+        )
+        print(
+            f"Predicted volumes shape: {predicted_volumes.shape}, dtype: {predicted_volumes.dtype}, min: {predicted_volumes.min()}, max: {predicted_volumes.max()}, mean: {predicted_volumes.mean()}, std: {predicted_volumes.std()}"
+        )
+        print(
+            f"Label volumes shape: {label_volumes.shape}, dtype: {label_volumes.dtype}, min: {label_volumes.min()}, max: {label_volumes.max()}, mean: {label_volumes.mean()}, std: {label_volumes.std()}"
+        )
+
+        # Start a Weights & Biases run
+        run = wandb.init(
+            project="mri-visualization", name="mri-visualization-test"
+        )
+
+        # Visualize the data
+        log_wandb_3d_volumes_and_masks(
+            input_volumes, predicted_volumes, label_volumes
+        )
+
+        # Finish the run
+        run.finish()
