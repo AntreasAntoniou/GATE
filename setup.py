613497189ff08132bb74efd04fb1c60b9dde0c40
Antreas Antoniou <iam@antreas.io>
2023-07-21 21:02:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ from setuptools import find_packages, setup
 print(f"Installing {find_packages()}")
 setup(
     name="gate",
-    version="0.8.6",
+    version="0.8.8",
     description="A minimal, stateless, machine learning research template for PyTorch",
     author="Antreas Antoniou",
     author_email="iam@antreas.io",
