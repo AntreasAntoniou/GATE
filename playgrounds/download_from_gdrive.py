aeb5e87adada43251f92e3445898905c82e1c795
Antreas Antoniou <a.antoniou@ed.ac.uk>
2023-10-11 13:26:02 +0100

    remove playground stuff

--- playgrounds/download_from_gdrive.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = "https://drive.google.com/uc?id=1hbzc_P1FuxMkcabkgn9ZKinBwW683j45"
-output = "output_filename.tgz"  # Here you can choose the output file name
-gdown.download(url, output, quiet=False)
