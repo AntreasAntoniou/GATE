3149d5fa7a906428f47713554c92e1872587da21
Antreas Antoniou <iam@antreas.io>
2023-11-06 06:16:06 +0000

    Update setup_variables.sh

--- secrets/setup_variables.sh
+++ secrets/setup_variables.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-export WANDB_API_KEY="821661c6ee1657a2717093701ab76574ae1a9be0"
-export WANDB_ENTITY=machinelearningbrewery
-export WANDB_PROJECT=gate-dev-0-8-12
+export WANDB_API_KEY=""
+export WANDB_ENTITY=""
+export WANDB_PROJECT=""
 
-export KAGGLE_USERNAME="antreasantoniou"
-export KAGGLE_KEY="d14aab63e71334cfa118bd5251bf85da"
+export KAGGLE_USERNAME=""
+export KAGGLE_KEY=""
 
-export DATASET_DIR="/disk/scratch_fast1/data/"
-export PROJECT_DIR="/disk/scratch_fast1/aantoni2/experiments/"
+export DATASET_DIR=""
+export PROJECT_DIR=""
 
-export EXPERIMENT_NAME=gate-dev-0-8-12
-export HF_USERNAME="Antreas"
-export HF_TOKEN=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
+export EXPERIMENT_NAME=""
+export HF_USERNAME=""
+export HF_TOKEN=""
 export HF_CACHE_DIR=$PYTEST_DIR
 
 export TOKENIZERS_PARALLELISM=False
