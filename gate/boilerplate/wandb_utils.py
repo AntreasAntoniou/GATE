* feat(data): update data preprocessing and visualization for medical datasets * refactor(models): update model selection and segmentation loss calculation * fix(menu): uncomment model selection options * chore: suppress warnings in medical decathlon dataset * refactor(run): remove unused import * test: update tests for medical datasets

gate/boilerplate/wandb_utils.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import math
 from typing import Dict, List, Optional, Union
@@ -46,6 +47,67 @@ def visualize_video_with_labels(video, logits, labels, name):
     return wandb_video_data_dict
 
 
+def visualize_volume(item, prefix: str):
+    input_volumes = item["image"].float()
+    predicted_volumes = item["labels"].float()
+    label_volumes = item["labels"].float()
+
+    # predicted_volumes[predicted_volumes == -1] = 10
+    # label_volumes[label_volumes == -1] = 10
+
+    print(
+        f"Input volumes shape: {input_volumes.shape}, dtype: {input_volumes.dtype}, min: {input_volumes.min()}, max: {input_volumes.max()}, mean: {input_volumes.mean()}, std: {input_volumes.std()}"
+    )
+    print(
+        f"Predicted volumes shape: {predicted_volumes.shape}, dtype: {predicted_volumes.dtype}, min: {predicted_volumes.min()}, max: {predicted_volumes.max()}, mean: {predicted_volumes.mean()}, std: {predicted_volumes.std()}"
+    )
+    print(
+        f"Label volumes shape: {label_volumes.shape}, dtype: {label_volumes.dtype}, min: {label_volumes.min()}, max: {label_volumes.max()}, mean: {label_volumes.mean()}, std: {label_volumes.std()}"
+    )
+
+    # Start a Weights & Biases run
+
+    target_size = 384
+    # Visualize the data
+    return log_wandb_3d_volumes_and_masks(
+        F.interpolate(
+            input_volumes.view(
+                -1,
+                input_volumes.shape[-3],
+                input_volumes.shape[-2],
+                input_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="bicubic",
+        ).view(*input_volumes.shape[:-2] + (target_size, target_size)),
+        F.interpolate(
+            predicted_volumes.view(
+                -1,
+                predicted_volumes.shape[-3],
+                predicted_volumes.shape[-2],
+                predicted_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="nearest-exact",
+        )
+        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
+        .long(),
+        F.interpolate(
+            label_volumes.view(
+                -1,
+                predicted_volumes.shape[-3],
+                predicted_volumes.shape[-2],
+                predicted_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="nearest-exact",
+        )
+        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
+        .long(),
+        prefix=prefix,
+    )
+
+
 def log_wandb_3d_volumes_and_masks(
     volumes: torch.Tensor,
     logits: torch.Tensor,
@@ -79,7 +141,7 @@ def log_wandb_3d_volumes_and_masks(
             label: str(label) for label in unique_labels
         }
 
-    print(
+    logger.debug(
         f"unique labels: {torch.unique(label_volumes)}, frequency: {torch.bincount(label_volumes.flatten())}"
     )
 
@@ -101,6 +163,9 @@ def log_wandb_3d_volumes_and_masks(
 
     # Log volumes to wandb
 
+    predicted_volumes = predicted_volumes.squeeze()
+    label_volumes = label_volumes.squeeze()
+
     bg_image = create_montage(input_volumes).permute([2, 0, 1]).float()
     prediction_mask = create_montage(predicted_volumes).long().squeeze()
     true_mask = create_montage(label_volumes).long().squeeze()
@@ -108,6 +173,10 @@ def log_wandb_3d_volumes_and_masks(
     bg_image = T.ToPILImage()(bg_image)
     prediction_mask = prediction_mask.cpu().numpy()
     true_mask = true_mask.cpu().numpy()
+
+    # print(
+    #     f"mask shape {prediction_mask.shape}, dtype: {prediction_mask.dtype}, min: {prediction_mask.min()}, max: {prediction_mask.max()}"
+    # )
 
     return {
         f"{prefix}/medical_segmentation_episode": [
@@ -167,16 +236,6 @@ def log_wandb_images(
         episode_list.append(ae_episode)
 
     return {f"{prefix}/autoencoder_episode": episode_list}
-
-
-import json
-from typing import Dict, List, Union
-
-import torch
-import torchvision.transforms as T
-from PIL import Image
-
-import wandb
 
 
 def log_wandb_image_classification(
@@ -265,14 +324,18 @@ def create_montage(arr: np.ndarray) -> np.ndarray:
     assert len(arr.shape) in [
         3,
         4,
+        5,
     ], "Input array should be 3D in the shape of (s, h, w) or 4D in the shape of (s, c, h, w)"
 
     # Get the shape of the input array
     if len(arr.shape) == 3:
         s, h, w = arr.shape
         c = None
-    else:
+    elif len(arr.shape) == 4:
         s, c, h, w = arr.shape
+    else:
+        b, s, c, h, w = arr.shape
+        arr = arr.reshape(-1, c, h, w)
 
     # Compute the new height and width
     h_new = w_new = math.ceil(math.sqrt(s))
@@ -313,11 +376,13 @@ def create_montage(arr: np.ndarray) -> np.ndarray:
     return torch.tensor(montage)
 
 
-def visualize_volume(item, name):
-    input_volumes = item["image"]
-    input_volumes = input_volumes.float().unsqueeze(0).unsqueeze(0)
-    predicted_volumes = item["labels"].float().unsqueeze(0)
-    label_volumes = item["labels"].float().unsqueeze(0)
+def visualize_volume(item, prefix: str, target_size: int = 384):
+    input_volumes = item["image"].float()
+    predicted_volumes = item["labels"].float()
+    label_volumes = item["labels"].float()
+
+    # predicted_volumes[predicted_volumes == -1] = 10
+    # label_volumes[label_volumes == -1] = 10
 
     print(
         f"Input volumes shape: {input_volumes.shape}, dtype: {input_volumes.dtype}, min: {input_volumes.min()}, max: {input_volumes.max()}, mean: {input_volumes.mean()}, std: {input_volumes.std()}"
@@ -330,28 +395,42 @@ def visualize_volume(item, name):
     )
 
     # Start a Weights & Biases run
-    run = wandb.init(
-        project="gate-visualization", job_type="visualize_dataset"
-    )
 
     # Visualize the data
-    wandb.log(
-        log_wandb_3d_volumes_and_masks(
-            F.interpolate(
-                input_volumes.reshape(
-                    -1,
-                    input_volumes.shape[-3],
-                    input_volumes.shape[-2],
-                    input_volumes.shape[-1],
-                ),
-                size=(256, 256),
-                mode="bicubic",
-            ).reshape(*input_volumes.shape[:-2] + (256, 256)),
-            predicted_volumes.long(),
-            label_volumes.long(),
-            prefix=name,
+    return log_wandb_3d_volumes_and_masks(
+        F.interpolate(
+            input_volumes.view(
+                -1,
+                input_volumes.shape[-3],
+                input_volumes.shape[-2],
+                input_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="bicubic",
+        ).view(*input_volumes.shape[:-2] + (target_size, target_size)),
+        F.interpolate(
+            predicted_volumes.view(
+                -1,
+                predicted_volumes.shape[-3],
+                predicted_volumes.shape[-2],
+                predicted_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="nearest-exact",
         )
+        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
+        .long(),
+        F.interpolate(
+            label_volumes.view(
+                -1,
+                predicted_volumes.shape[-3],
+                predicted_volumes.shape[-2],
+                predicted_volumes.shape[-1],
+            ),
+            size=(target_size, target_size),
+            mode="nearest-exact",
+        )
+        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
+        .long(),
+        prefix=prefix,
     )
-
-    # Finish the run
-    run.finish()

gate/data/medical/segmentation/medical_decathlon.py
@@ -1,11 +1,11 @@
 import logging
 import multiprocessing as mp
+import warnings
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any, Dict, List, Optional, Union
 
 import datasets
-import monai
 import numpy as np
 import torch
 import torchvision.transforms as T
@@ -24,6 +24,13 @@ from gate.data.transforms.segmentation import (
     MedicalImageSegmentationTransforms,
     PhotometricParams,
 )
+
+# Ignore all DeprecationWarnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Ignore all UserWarnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
 
 logger = logging.getLogger(__name__)
 logging.getLogger("monai").setLevel(logging.CRITICAL)
@@ -86,6 +93,7 @@ def build_dataset(
     Returns:
         A dictionary containing the dataset split.
     """
+    import monai
 
     logger.info(
         f"Loading Medical Decathlon {task_name} dataset, will download to {data_dir} if necessary."
@@ -101,7 +109,7 @@ def build_dataset(
         num_workers=mp.cpu_count(),
         progress=True,
         cache_num=0,
-        cache_rate=0.0,
+        cache_rate=1.0,
         copy_cache=False,
         as_contiguous=True,
         runtime_cache=False,
@@ -138,6 +146,29 @@ def patient_normalization(input_volume):
     )
 
     return input_volume
+
+
+def convert_to_b3hw(image):
+    image_shape_len = len(image.shape)
+
+    if image_shape_len == 4 and image.shape[1] == 4:  # Case of (b, 4, h, w)
+        # calculate the average contribution of alpha channel
+        alpha_contrib = image[:, 3:4, :, :] / 3.0
+        rgb = image[:, :3, :, :]
+        # Add alpha contribution to RGB channels, and clip values to be in [0,1]
+        image = rgb + alpha_contrib
+    elif image_shape_len == 4 and image.shape[1] == 1:  # Case of (b, 1, h, w)
+        # Simply repeat the single channel 3 times
+        image = image.expand(-1, 3, -1, -1)
+    elif image_shape_len == 3:  # Case of (b, h, w)
+        # Expand dimensions before repeating the single channel 3 times
+        image = image.unsqueeze(1).expand(-1, 3, -1, -1)
+    else:  # Case of (b, 3, h, w). Leave as is if already in the desired format
+        raise ValueError(
+            f"Invalid image shape {image.shape}, should be one of (b, 4, h, w), (b, 1, h, w), (b, h, w), (b, 3, h, w)"
+        )
+
+    return image
 
 
 class DatasetTransforms:
@@ -204,7 +235,10 @@ class DatasetTransforms:
             image = image.permute(2, 0, 1).unsqueeze(1)
             annotation = annotation.permute(2, 0, 1)
 
-        print(f"input shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"input shapes {image.shape}, {annotation.shape}")
+
+        image = torch.tensor(image)
+        annotation = torch.tensor(annotation)
 
         image = T.Resize(
             (self.initial_size[0], self.initial_size[1]),
@@ -220,17 +254,17 @@ class DatasetTransforms:
 
         annotation = annotation.unsqueeze(1)
 
-        print(f"pre crop shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"pre crop shapes {image.shape}, {annotation.shape}")
 
-        # if self.crop_size is not None:
-        #     image, annotation = self.crop_transform(image, annotation)
+        if self.crop_size is not None:
+            image, annotation = self.crop_transform(image, annotation)
 
-        print(f"post crop shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"post crop shapes {image.shape}, {annotation.shape}")
 
-        # if self.med_transforms is not None:
-        #     image, annotation = self.med_transforms(image, annotation)
+        if self.med_transforms is not None:
+            image, annotation = self.med_transforms(image, annotation)
 
-        print(f"post med shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"post med shapes {image.shape}, {annotation.shape}")
 
         image = T.Resize(
             (self.input_size[0], self.input_size[1]),
@@ -244,16 +278,17 @@ class DatasetTransforms:
             antialias=False,
         )(annotation)
 
-        print(f"post resize shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"post resize shapes {image.shape}, {annotation.shape}")
 
+        image = convert_to_b3hw(image)
         image = patient_normalization(image)
         annotation = annotation.long()
 
-        print(
+        logger.debug(
             f"unique annotation values {torch.unique(annotation)}, frequency {torch.bincount(annotation.flatten())}",
         )
 
-        print(f"post norm shapes {image.shape}, {annotation.shape}")
+        logger.debug(f"post norm shapes {image.shape}, {annotation.shape}")
 
         return {
             "image": image,
@@ -267,7 +302,7 @@ def build_gate_dataset(
     task_name: str = "task01braintumour",
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -276,7 +311,6 @@ def build_gate_dataset(
         label_size=label_image_size,
         initial_size=train_initial_size,
         crop_size=image_size,
-        photometric_config=PhotometricParams(),
     )
     eval_transforms = DatasetTransforms(
         input_size=image_size,
@@ -330,7 +364,7 @@ def build_gate_md_brain_tumour(
     task_name=TaskOptions.BrainTumour.value,
     image_size: int = 256,
     label_image_size: int = 256,
-    train_initial_size: int = 384,
+    train_initial_size: int = 320,
     eval_initial_size: int = 256,
     ignore_index=-1,
 ) -> dict:
@@ -357,7 +391,7 @@ def build_gate_md_heart(
     task_name=TaskOptions.Heart.value,
     image_size: int = 320,
     label_image_size: int = 256,
-    train_initial_size: int = 512,
+    train_initial_size: int = 384,
     eval_initial_size: int = 320,
     ignore_index=-1,
 ) -> dict:
@@ -384,7 +418,7 @@ def build_gate_md_liver(
     task_name=TaskOptions.Liver.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -411,7 +445,7 @@ def build_gate_md_hippocampus(
     task_name=TaskOptions.Hippocampus.value,
     image_size: int = 256,
     label_image_size: int = 256,
-    train_initial_size: int = 512,
+    train_initial_size: int = 320,
     eval_initial_size: int = 256,
     ignore_index=-1,
 ) -> dict:
@@ -438,7 +472,7 @@ def build_gate_md_prostate(
     task_name=TaskOptions.Prostate.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -465,7 +499,7 @@ def build_gate_md_lung(
     task_name=TaskOptions.Lung.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -492,7 +526,7 @@ def build_gate_md_pancreas(
     task_name=TaskOptions.Pancreas.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -519,7 +553,7 @@ def build_gate_md_hepatic_vessel(
     task_name=TaskOptions.HepaticVessel.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -546,7 +580,7 @@ def build_gate_md_spleen(
     task_name=TaskOptions.Spleen.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:
@@ -573,7 +607,7 @@ def build_gate_md_colon(
     task_name=TaskOptions.Colon.value,
     image_size: int = 512,
     label_image_size: int = 256,
-    train_initial_size: int = 1024,
+    train_initial_size: int = 640,
     eval_initial_size: int = 512,
     ignore_index=-1,
 ) -> dict:

gate/models/task_adapters/semantic_segmentation.py
@@ -9,7 +9,6 @@ from typing import Callable, Dict, List, Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from datasets.table import pa
 
 from gate.boilerplate.decorators import ensemble_marker
 from gate.metrics.segmentation import (
@@ -86,7 +85,7 @@ def md_optimization_loss(
         logits: (B, C, H, W)
         labels: (B, 1, H, W)
     """
-    dice_loss_fn = DiceLoss()
+    dice_loss_fn = DiceLoss(ignore_index=ignore_index)
 
     dice_loss = dice_loss_fn.forward(logits, labels)
 
@@ -110,11 +109,11 @@ def md_optimization_loss(
 
     background_ce_loss = background_ce_loss_fn.forward(logits, labels)
 
-    loss = dice_loss + ce_loss + 0.01 * background_ce_loss
+    loss = dice_loss + 0.1 * ce_loss
     background_loss = background_dice_loss
 
     return {
-        "loss": loss + 0.5 * background_loss,
+        "loss": loss,
         "ce_loss": ce_loss,
         "dice_loss": dice_loss,
         "focal_loss": focal_loss,

gate/models/task_specific_models/semantic_segmentation/timm.py
@@ -12,6 +12,7 @@ from gate.config.variables import (
     HYDRATED_NUM_CLASSES,
     HYDRATED_TASK_NAME,
 )
+from gate.data.image.segmentation.classes import acdc_labels
 from gate.models import ModelAndTransform
 from gate.models.backbones.timm import TimmCLIPAdapter
 from gate.models.core import (
@@ -23,7 +24,6 @@ from gate.models.task_adapters.semantic_segmentation import (
     SegmentationAdapter,
     SegmentationAdapterOptions,
     SegmentationLossOptions,
-    VolumeSegmentationAdapter,
 )
 
 # modality_a_model: nn.Module,
@@ -132,6 +132,54 @@ def build_gate_md_model(
     if isinstance(num_classes, dict) or isinstance(num_classes, DictConfig):
         num_classes = len(num_classes[task_name])
 
+    model_and_transform = build_model(
+        timm_model_name=timm_model_name,
+        clip_model_name=clip_model_name,
+        pretrained=pretrained,
+        decoder_num_blocks=decoder_depth,
+        decoder_num_heads=decoder_num_heads,
+        num_classes=num_classes,
+        image_size=image_size,
+        decoder_layer_type=decoder_layer_type,
+        ignore_index=ignore_index,
+        background_loss_weight=background_loss_weight,
+        loss_type_id=SegmentationLossOptions.MD.value,
+    )
+
+    model_modality_config_image_classification = TargetModalityConfig(
+        image=[SourceModalityConfig(image=True)]
+    )
+
+    gate_model = GATEModel(
+        config=model_modality_config_image_classification,
+        model=model_and_transform.model,
+    )
+
+    return ModelAndTransform(
+        model=gate_model, transform=model_and_transform.transform
+    )
+
+
+@configurable(
+    group="model",
+    name="timm-acdc-segmentation-transformer",
+    defaults=dict(
+        num_classes=HYDRATED_NUM_CLASSES,
+        image_size=HYDRATED_IMAGE_SIZE,
+    ),
+)
+def build_gate_acdc_model(
+    timm_model_name: str = "resnet50.a1_in1k",
+    clip_model_name: str = "openai/clip-vit-base-patch16",
+    pretrained: bool = True,
+    decoder_depth: int = 2,
+    decoder_num_heads: int = 8,
+    num_classes: int = len(acdc_labels),
+    image_size: int = 512,
+    decoder_layer_type: str = SegmentationAdapterOptions.TRANSFORMER.value,
+    ignore_index: int = 0,
+    background_loss_weight: float = 0.01,
+):
     model_and_transform = build_model(
         timm_model_name=timm_model_name,
         clip_model_name=clip_model_name,

playground/medical-decathlon-monai.py
@@ -6,8 +6,8 @@ import rich
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as T
-
 import wandb
+
 from gate.boilerplate.wandb_utils import log_wandb_3d_volumes_and_masks
 
 # Load environment variables from .env file

tests/data/medical/test_automated_cardiac_diagnosis.py
@@ -3,7 +3,10 @@ import os
 import torch.nn.functional as F
 import wandb
 
-from gate.boilerplate.utils import log_wandb_3d_volumes_and_masks
+from gate.boilerplate.wandb_utils import (
+    log_wandb_3d_volumes_and_masks,
+    visualize_volume,
+)
 from gate.data.medical.segmentation.automated_cardiac_diagnosis import (
     build_dataset,
     build_gate_dataset,
@@ -47,45 +50,8 @@ def test_build_gate_dataset():
         break
 
 
-def visualize_volume(item):
-    input_volumes = item["image"]
-    input_volumes = input_volumes.float()
-    predicted_volumes = item["labels"].float()
-    label_volumes = item["labels"].float()
-
-    predicted_volumes[predicted_volumes == -1] = 10
-    label_volumes[label_volumes == -1] = 10
-
-    print(
-        f"Input volumes shape: {input_volumes.shape}, dtype: {input_volumes.dtype}, min: {input_volumes.min()}, max: {input_volumes.max()}, mean: {input_volumes.mean()}, std: {input_volumes.std()}"
-    )
-    print(
-        f"Predicted volumes shape: {predicted_volumes.shape}, dtype: {predicted_volumes.dtype}, min: {predicted_volumes.min()}, max: {predicted_volumes.max()}, mean: {predicted_volumes.mean()}, std: {predicted_volumes.std()}"
-    )
-    print(
-        f"Label volumes shape: {label_volumes.shape}, dtype: {label_volumes.dtype}, min: {label_volumes.min()}, max: {label_volumes.max()}, mean: {label_volumes.mean()}, std: {label_volumes.std()}"
-    )
-
-    # Start a Weights & Biases run
-    run = wandb.init(
-        project="gate-visualization", job_type="visualize_dataset"
-    )
-
-    # Visualize the data
-    wandb.log(
-        log_wandb_3d_volumes_and_masks(
-            F.interpolate(
-                input_volumes.reshape(-1, input_volumes.shape[-3], 512, 512),
-                size=(256, 256),
-                mode="bicubic",
-            ).reshape(*input_volumes.shape[:-2] + (256, 256)),
-            predicted_volumes.long(),
-            label_volumes.long(),
-        )
-    )
-
-
 def test_build_gate_visualize_dataset():
+    wandb.init(project="gate_visualization_pytest")
     gate_dataset = build_gate_dataset(data_dir=os.environ.get("PYTEST_DIR"))
     assert gate_dataset["train"] is not None, "Train set should not be None"
     assert gate_dataset["val"] is not None, "Validation set should not be None"
@@ -95,19 +61,19 @@ def test_build_gate_visualize_dataset():
         print(list(item.keys()))
         assert item["image"] is not None, "Image should not be None"
         assert item["labels"] is not None, "Label should not be None"
-        wandb.log(visualize_volume(item))
+        wandb.log(visualize_volume(item, prefix="train-acdc"))
         break
 
     for item in gate_dataset["val"]:
         print(list(item.keys()))
         assert item["image"] is not None, "Image should not be None"
         assert item["labels"] is not None, "Label should not be None"
-        wandb.log(visualize_volume(item))
+        wandb.log(visualize_volume(item, prefix="val-acdc"))
         break
 
     for item in gate_dataset["test"]:
         print(list(item.keys()))
         assert item["image"] is not None, "Image should not be None"
         assert item["labels"] is not None, "Label should not be None"
-        wandb.log(visualize_volume(item))
+        wandb.log(visualize_volume(item, prefix="test-acdc"))
         break

tests/data/medical/test_segmentation_decathlon.py
@@ -6,9 +6,12 @@ from enum import Enum
 import pytest
 import torch.nn.functional as F
 import torchvision.transforms as T
-
 import wandb
-from gate.boilerplate.wandb_utils import log_wandb_3d_volumes_and_masks
+
+from gate.boilerplate.wandb_utils import (
+    log_wandb_3d_volumes_and_masks,
+    visualize_volume,
+)
 from gate.data.medical.segmentation.medical_decathlon import (
     build_dataset,
     build_gate_md_brain_tumour,
@@ -69,67 +72,6 @@ class TaskOptions(Enum):
 #         assert item["image"] is not None, "Image should not be None"
 #         assert item["labels"] is not None, "Label should not be None"
 #         break
-
-
-def visualize_volume(item, prefix: str):
-    input_volumes = item["image"].float()
-    predicted_volumes = item["labels"].float()
-    label_volumes = item["labels"].float()
-
-    # predicted_volumes[predicted_volumes == -1] = 10
-    # label_volumes[label_volumes == -1] = 10
-
-    print(
-        f"Input volumes shape: {input_volumes.shape}, dtype: {input_volumes.dtype}, min: {input_volumes.min()}, max: {input_volumes.max()}, mean: {input_volumes.mean()}, std: {input_volumes.std()}"
-    )
-    print(
-        f"Predicted volumes shape: {predicted_volumes.shape}, dtype: {predicted_volumes.dtype}, min: {predicted_volumes.min()}, max: {predicted_volumes.max()}, mean: {predicted_volumes.mean()}, std: {predicted_volumes.std()}"
-    )
-    print(
-        f"Label volumes shape: {label_volumes.shape}, dtype: {label_volumes.dtype}, min: {label_volumes.min()}, max: {label_volumes.max()}, mean: {label_volumes.mean()}, std: {label_volumes.std()}"
-    )
-
-    # Start a Weights & Biases run
-
-    target_size = 384
-    # Visualize the data
-    return log_wandb_3d_volumes_and_masks(
-        F.interpolate(
-            input_volumes.view(
-                -1,
-                input_volumes.shape[-3],
-                input_volumes.shape[-2],
-                input_volumes.shape[-1],
-            ),
-            size=(target_size, target_size),
-            mode="bicubic",
-        ).view(*input_volumes.shape[:-2] + (target_size, target_size)),
-        F.interpolate(
-            predicted_volumes.view(
-                -1,
-                predicted_volumes.shape[-3],
-                predicted_volumes.shape[-2],
-                predicted_volumes.shape[-1],
-            ),
-            size=(target_size, target_size),
-            mode="nearest-exact",
-        )
-        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
-        .long(),
-        F.interpolate(
-            label_volumes.view(
-                -1,
-                predicted_volumes.shape[-3],
-                predicted_volumes.shape[-2],
-                predicted_volumes.shape[-1],
-            ),
-            size=(target_size, target_size),
-            mode="nearest-exact",
-        )
-        .view(*predicted_volumes.shape[:-2] + (target_size, target_size))
-        .long(),
-        prefix=prefix,
-    )
 
 
 @pytest.mark.parametrize(