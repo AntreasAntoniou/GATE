de8cb27de07e81940bbd0c91e878c8394029c70f
Antreas Antoniou <iam@antreas.io>
2023-12-05 01:21:33 +0000

    * chore(setup.py): update version from 0.9.0 to 0.9.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 print(f"Installing {find_packages()}")
 setup(
     name="gate",
-    version="0.9.0",
+    version="0.9.1",
     description="A minimal, stateless, machine learning research template for PyTorch",
     author="Antreas Antoniou",
     author_email="iam@antreas.io",
