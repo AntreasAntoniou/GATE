3724ea3f57e98ff87ad558a73fdd0e8eef65bee8
Antreas Antoniou <iam@antreas.io>
2023-12-10 10:47:45 +0000

    * feat(image classification): enable F101 and HAPPYWHALE datasets in DatasetName enum

--- gate/data/image/classification/__init__.py
+++ gate/data/image/classification/__init__.py
@@ -4,8 +4,8 @@ from enum import Enum
 class DatasetName(Enum):
     IN1K = "imagenet1k-classification"
     C100 = "cifar100"
-    # F101 = "food101"
+    F101 = "food101"
     # STL10 = "stl10"
     # SVHN = "svhn"
     P365 = "places365"
-    # HAPPYWHALE = "happy_whale_dolphin_classification"
+    HAPPYWHALE = "happy_whale_dolphin_classification"
