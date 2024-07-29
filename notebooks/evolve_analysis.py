6a31aa5cc22092afc6dad2ca5afb14fc073707c1
Antreas Antoniou <iam@antreas.io>
2024-07-29 02:47:40 +0100

    clean up

--- notebooks/evolve_analysis.py
+++
@@ -1,9 +0,0 @@
-import wandb
-
-run = wandb.init()
-artifact = run.use_artifact(
-    "machinelearningbrewery/combination-optimization/top_combinations:v99",
-    type="top_combinations",
-)
-artifact_dir = artifact.download()
-print(artifact_dir)
